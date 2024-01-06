a818cc5f948d93ff15de0b25323482a4d04aaeca
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2024-01-06 19:03:45 +0530

    Time: 1 ms (99.10%) | Memory: 46.2 MB (5.89%) - LeetSync

--- leetcode/724-find-pivot-index/find-pivot-index.java
+++ leetcode/724-find-pivot-index/find-pivot-index.java
@@ -1,26 +1,25 @@
 class Solution {
-    public int pivotIndex(int[] nums) {
-        int[] prefixSum=prefSum(nums);
-        for(int i=0;i<nums.length;i++){
-            int ls=0,rs=0;
-        if(i!=0){
-           ls=prefixSum[i-1];
-        }
-        if(i!=nums.length-1){
-           rs=prefixSum[nums.length-1]-prefixSum[i];
-        }
-        if(ls==rs){
-            return i;
-        }
-        }
-        return -1;
+    public static int[] sumUp(int[] nums){
+            int[] ans=new int[nums.length];
+            ans[0]=nums[0];
+            for(int i=1;i<nums.length;i++){
+                ans[i]=ans[i-1]+nums[i];
+            }
+            return ans;
     }
-    public int[] prefSum(int[] nums){
-        int[] ans=new int[nums.length];
-        ans[0]=nums[0];
-        for(int i=1;i<nums.length;i++){
-            ans[i]=nums[i]+ans[i-1];
-        }
-        return ans;
+    public int pivotIndex(int[] nums) {
+       int[] arr=sumUp(nums);
+       if(arr[arr.length-1]-arr[0]==0){
+           return 0;
+       }
+       for(int i=1;i<arr.length-1;i++){
+          if(arr[i-1]==(arr[arr.length-1]-arr[i])){
+              return i;
+          }
+       }
+       if(arr[arr.length-2]==0){
+           return arr.length-1;
+       }
+       return -1;
     } 
 }
