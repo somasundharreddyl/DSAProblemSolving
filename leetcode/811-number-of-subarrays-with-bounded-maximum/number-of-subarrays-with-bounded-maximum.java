6adc8796d0f672884435561e7bb485f263e305bf
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2024-01-09 13:02:20 +0530

    Time: 4 ms (29.03%) | Memory: 55 MB (51.27%) - LeetSync

--- leetcode/811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.java
+++ leetcode/811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.java
@@ -1,18 +1,18 @@
 class Solution {
     public int numSubarrayBoundedMax(int[] nums, int left, int right) {
-        int ans=0,previousvalidStartingPoints=0,lgri=0;
-        for(int i=0;i<nums.length;i++){
-        if(nums[i]>right){
-        lgri=i+1;
-        previousvalidStartingPoints=0;
-        }
-        else if(nums[i]>=left && nums[i]<=right){
-          ans+=(i-lgri+1);
-          previousvalidStartingPoints= i-lgri+1;
-        }else{
-            ans+=previousvalidStartingPoints;
-        }
-        }
-        return ans;
+        int ans=0,validSPI=0,totalPrevSP=0;   //SPI=StartingPointIndex
+       for(int i=0;i<nums.length;i++){
+           if(nums[i]>right){
+             validSPI=i+1;
+             totalPrevSP=0;
+           }
+           else if(nums[i]>=left && nums[i]<=right){
+               ans=ans+(i-validSPI+1);
+               totalPrevSP=i-validSPI+1;
+           }else{
+               ans=ans+totalPrevSP;
+           }
+       } 
+       return ans;
     }
 }
