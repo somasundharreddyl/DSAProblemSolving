ac6659939c4107253bbac3339fe100ddafd8bdf6
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2024-01-10 12:09:06 +0530

    Time: 106 ms (36.87%) | Memory: 68.5 MB (31.66%) - LeetSync

--- leetcode/304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ leetcode/304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,41 +1,41 @@
 class NumMatrix {
-
-int[][] prefixSum;
+ 
+   int[][] prefixSumArr;
     public NumMatrix(int[][] matrix) {
-
-     prefixSum=new int[matrix.length][matrix[0].length];
-     int m=matrix.length,n=matrix[0].length;
-
-     for(int i=0;i<m;i++) {
-         for(int j=0;j<n;j++){
-             if(j==0){prefixSum[i][j]=matrix[i][j];}
-             else{
-                prefixSum[i][j]=prefixSum[i][j-1]+matrix[i][j];
+     int row=matrix.length;
+     int col=matrix[0].length;
+     prefixSumArr=new int[row][col];
+      
+     for(int i=0;i<row;i++){
+         for(int j=0;j<col;j++){
+             if(j==0){
+           prefixSumArr[i][j]=matrix[i][j];
+             }else{
+                 prefixSumArr[i][j]=prefixSumArr[i][j-1]+matrix[i][j];
              }
          }
      }
 
-    for(int i=0;i<n;i++) {
-         for(int j=1;j<m;j++){
-                prefixSum[j][i]=prefixSum[j-1][i]+prefixSum[j][i];
-             }
+     for(int i=0;i<col;i++){
+         for(int j=1;j<row;j++){
+             prefixSumArr[j][i]+=prefixSumArr[j-1][i];
          }
-     
+     }
 
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum=prefixSum[row2][col2];
+        int ans=prefixSumArr[row2][col2];
         if(row1-1>=0){
-          sum=sum-prefixSum[row1-1][col2];
+           ans=ans-prefixSumArr[row1-1][col2];
         }
-        if(col1-1>=0){
-          sum=sum-prefixSum[row2][col1-1];
+         if(col1-1>=0){
+           ans=ans-prefixSumArr[row2][col1-1];
         }
-        if(row1-1>=0&&col1-1>=0){
-            sum=sum+prefixSum[row1-1][col1-1];
+        if(row1-1>=0 && col1-1>=0){
+           ans=ans+prefixSumArr[row1-1][col1-1];
         }
-        return sum;
+        return ans;
     }
 }
 
