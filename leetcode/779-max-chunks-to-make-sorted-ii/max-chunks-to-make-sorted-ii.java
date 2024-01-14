23a46f92d9c53954207b099fa25b588707d923a9
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2024-01-14 07:46:54 +0530

    Time: 1 ms (43.87%) | Memory: 44.5 MB (18.63%) - LeetSync

--- leetcode/779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.java
+++ leetcode/779-max-chunks-to-make-sorted-ii/max-chunks-to-make-sorted-ii.java
@@ -1,30 +1,33 @@
 class Solution {
-    public int[] prefix(int[] arr){
-     int[] prefix=new int[arr.length];
-     prefix[0]=arr[0];
-     for(int i=1;i<arr.length;i++){
-         prefix[i]=prefix[i-1]>arr[i]?prefix[i-1]:arr[i];
-     }
-     return prefix;
+
+    public int[] prefixMax(int[] arr){
+      int[] nums=new int[arr.length];
+      nums[0]=arr[0];
+      for(int i=1;i<nums.length;i++){
+          nums[i]=nums[i-1]>arr[i]?nums[i-1]:arr[i];
+      }
+      return nums;
     }
-    public int[] suffix(int[] arr){
-        int[] suffix=new int[arr.length];
-     suffix[arr.length-1]=arr[arr.length-1];
-     for(int i=arr.length-2;i>=0;i--){
-         suffix[i]=suffix[i+1]<arr[i]?suffix[i+1]:arr[i];
-     }
-     return suffix;
+
+    public int[] suffixMin(int[] arr){
+      int[] nums=new int[arr.length];
+      nums[arr.length-1]=arr[arr.length-1];
+      for(int i=arr.length-2;i>=0;i--){
+          nums[i]=nums[i+1]<arr[i]?nums[i+1]:arr[i];
+      }
+      return nums;
     }
+    
     public int maxChunksToSorted(int[] arr) {
-        int count=0;
-        int[] prefixMaxArr=prefix(arr);
-        int[] suffixMinArr=suffix(arr);
-        for(int i=0;i<arr.length-1;i++){
-            if(prefixMaxArr[i]<=suffixMinArr[i+1]){
+       int[] prefMax=prefixMax(arr);
+       int[] suffMin=suffixMin(arr);
+       int count=0;
+       for(int i=0;i<arr.length-1;i++){
+           if(prefMax[i]<=suffMin[i+1]){
                count++;
-            }
-        }
-        count++;
-        return count;
+           }
+       }
+       return ++count;
+
     }
 }
