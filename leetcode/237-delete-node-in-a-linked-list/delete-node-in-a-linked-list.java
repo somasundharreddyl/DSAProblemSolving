db84ba417e95d3d83bc03f2c380aff2ac654561c
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2024-02-04 18:09:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (59.57%) - LeetSync

--- leetcode/237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
+++ leetcode/237-delete-node-in-a-linked-list/delete-node-in-a-linked-list.java
@@ -8,8 +8,8 @@
  */
 class Solution {
     public void deleteNode(ListNode node) {
-       ListNode temp=node;
-       temp.val=temp.next.val;
-       temp.next=temp.next.next;
+        ListNode temp=node;
+        temp.val=temp.next.val;
+        temp.next=temp.next.next;
     }
 }
