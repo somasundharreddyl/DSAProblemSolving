26c64dd8304c57326889bf433d54090f7f92a2ce
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2024-02-05 08:27:56 +0530

    Time: 2 ms (64.68%) | Memory: 41.4 MB (42.59%) - LeetSync

--- leetcode/20-valid-parentheses/valid-parentheses.java
+++ leetcode/20-valid-parentheses/valid-parentheses.java
@@ -2,23 +2,19 @@ class Solution {
     public boolean isValid(String s) {
         Stack<Character> st=new Stack<>();
         for(int i=0;i<s.length();i++){
-            if(st.size()==0 || s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{'){
+            if(st.size()<=0){
                 st.push(s.charAt(i));
-            }else if(s.charAt(i)==')'){
-               if(st.peek()!='('){return false;}
-               else{st.pop();}
-            }else if(s.charAt(i)==']'){
-               if(st.peek()!='['){return false;}
-               else{st.pop();}
+            }
+            else if(st.peek()=='(' && s.charAt(i)==')' || 
+            st.peek()=='{' && s.charAt(i)=='}' || st.peek()=='[' && s.charAt(i)==']'){
+                st.pop();
             }else{
-               if(st.peek()!='{'){return false;}
-               else{st.pop();}
-            } 
+                st.push(s.charAt(i));
+            }
         }
-        if(st.size()==0){
-            return true;
-        }else{
+        if(st.size()>0){
             return false;
         }
+        return true;
     }
 }
