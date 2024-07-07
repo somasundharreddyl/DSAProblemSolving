bfc718784a27ce75353dc6fa92049f17de5ce072
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2024-07-07 08:55:37 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (23.25%) - LeetSync

--- leetcode/50-powx-n/powx-n.java
+++ leetcode/50-powx-n/powx-n.java
@@ -1,24 +1,28 @@
 class Solution {
-     public double myPow(double x, int n) {
-         long n1=n;
+
+    public double power(double x, long n1){
+    
+        if(n1==1){
+            return x;
+        }
+        if(n1==0){
+            return 1;
+        }
+        double temp=myPow(x, (int)(n1 / 2L));
+         if(n1%2==1){
+           return x*temp*temp; 
+        }else{
+        return temp*temp;
+        }
+        
+    }
+
+    public double myPow(double x, int n) {
+        long n1=n;
         if(n1<0){
            n1=n1*(-1);
            x=1/x;
         }
-        return pow(x,n1);
-}
-    public double pow(double x, long n){
-       if(n==1){
-            return x;
-        }
-        if(n==0){
-            return 1;
-        }
-        double temp=pow(x,n/2);
-        if(n%2==0){
-            return temp*temp;
-        }else{
-            return x*temp*temp;
+        return power(x,n1);
         }
     }
-}
