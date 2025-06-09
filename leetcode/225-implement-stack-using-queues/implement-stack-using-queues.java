738f39293a85df7fc36cb7ce23403ecd5a94672b
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-06-09 06:26:49 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (96.26%) - LeetSync

--- leetcode/225-implement-stack-using-queues/implement-stack-using-queues.java
+++ leetcode/225-implement-stack-using-queues/implement-stack-using-queues.java
@@ -1,36 +1,33 @@
 class MyStack {
-Queue<Integer> q=new LinkedList<>();
+    Queue<Integer> q1;
     public MyStack() {
-        
+       q1=new LinkedList<>(); 
     }
     
     public void push(int x) {
-        q.add(x);
+        q1.add(x);
     }
     
     public int pop() {
-        
-        for(int i=1;i<q.size();i++){
-            q.add(q.remove());
-        }
-        return q.remove();    
+       int size=q1.size();
+       for(int i=0;i<size-1;i++){
+        q1.add(q1.remove());
+       }
+       return q1.remove(); 
     }
     
     public int top() {
-        for(int i=1;i<q.size();i++){
-            q.add(q.remove());
-        }
-        int ans=q.remove();
-         q.add(ans);
-        return ans;
+        int size=q1.size();
+       for(int i=0;i<size-1;i++){
+        q1.add(q1.remove());
+       }
+       int res=q1.remove();
+       q1.add(res);
+       return res;
     }
     
     public boolean empty() {
-        if(q.size()==0){
-            return true;
-        }else{
-            return false;
-        }
+        return q1.isEmpty();
     }
 }
 
