7f588d8ce8532faaaba8ed8c9f5126e7a9045cb1
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-06-11 09:19:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (26.25%) - LeetSync

--- leetcode/62-unique-paths/unique-paths.java
+++ leetcode/62-unique-paths/unique-paths.java
@@ -1,29 +1,18 @@
 class Solution {
-
-    public int totalways(int[][] dp,int i,int j){
-
-        if(i<0 || j<0){
-            return 0;
-        }
-        if(i==0&&j==0){
+    public int totalPaths(int m, int n, int[][] dp){
+          if(m==0 || n==0){
             return 1;
-        }
-        if(dp[i][j]!=-1){return dp[i][j];}
-    
-    int a=totalways(dp,i-1,j);
-    int b=totalways(dp,i,j-1);
-
-    dp[i][j]=a+b;
-    return a+b;
+          }
+          if(dp[m][n]!=-1){return dp[m][n];}
+          dp[m][n]=totalPaths(m-1,n,dp)+totalPaths(m,n-1,dp);
+          return totalPaths(m-1,n,dp)+totalPaths(m,n-1,dp);
 
     }
-
     public int uniquePaths(int m, int n) {
         int[][] dp=new int[m][n];
-         for(int i=0;i<m;i++){
-             Arrays.fill(dp[i],-1);
-         }
-        int ans=totalways(dp,m-1,n-1);
-        return ans;
+        for(int[] arr:dp){
+            Arrays.fill(arr,-1);
+        }
+        return totalPaths(m-1,n-1,dp);
     }
 }
