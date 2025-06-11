d3638cb06e1e0d034f6aab29dbded703c7710b24
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-06-11 22:09:34 +0530

    Time: 1 ms (62.33%) | Memory: 46.3 MB (59.50%) - LeetSync

--- leetcode/42-trapping-rain-water/trapping-rain-water.java
+++ leetcode/42-trapping-rain-water/trapping-rain-water.java
@@ -1,30 +1,26 @@
 class Solution {
-    private int[] findPrefMax(int[] height){
-      int[] arr=new int[height.length];
-      arr[0]=height[0];
-      for(int i=1;i<height.length;i++){
-          arr[i]=arr[i-1]>height[i]?arr[i-1]:height[i];
-      }
-      return arr;
-    }
-    private int[] findSuffMax(int[] height){
-     int[] arr=new int[height.length];
-       arr[height.length-1]=height[height.length-1]; 
-       for(int i=height.length-2;i>=0;i--){
-          arr[i]=arr[i+1]>height[i]?arr[i+1]:height[i]; 
-       }
-       return arr;
-    }
     public int trap(int[] height) {
-        int[] prefMax=findPrefMax(height);
-        int[] suffMax=findSuffMax(height);
-       int ans=0;
-       for(int i=1;i<height.length-1;i++){
-           int units=Math.min(prefMax[i-1],suffMax[i+1])-height[i];
-           if(units>0){
-          ans+=units;
-           }
-       }
-       return ans;
+        int[] prefixMax=new int[height.length];
+        int[] suffixMax=new int[height.length];
+        int ans=0;
+        prefixMax[0]=height[0];
+        for(int i=1;i<height.length;i++){
+          prefixMax[i]=height[i]>prefixMax[i-1]?height[i]:prefixMax[i-1];
+        }
+        suffixMax[height.length-1]=height[height.length-1];
+        for(int i=height.length-2;i>=0;i--){
+           suffixMax[i]=height[i]>suffixMax[i+1]?height[i]:suffixMax[i+1];
+        }
+        for(int i=1;i<height.length-1;i++){
+            int min=Math.min(prefixMax[i-1],suffixMax[i+1]);
+            if(min>height[i]){
+                ans=ans+min-height[i];
+            } 
+        }
+        return ans;
     }
 }
+
+//4,4,4,4,4,5
+//5,5,5,5,5,5
+//2+4+1+2
