1d5102eb3fac9d0e2a60654d8e73fedfdac8e130
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-06-11 12:27:48 +0530

    Time: 1 ms (99.76%) | Memory: 47.7 MB (57.10%) - LeetSync

--- leetcode/64-minimum-path-sum/minimum-path-sum.java
+++ leetcode/64-minimum-path-sum/minimum-path-sum.java
@@ -1,25 +1,20 @@
 class Solution {
-    public int minPathSum(int[][] grid) {
-        int m=grid.length;
-        int n=grid[0].length;
-        int[][] dp=new int[m][n];
-        for(int i=0;i<m;i++){
-            Arrays.fill(dp[i],-1);
+    public int minPath(int[][] grid,int i, int j, int[][] dp){
+        if(i<0 || j<0){
+            return Integer.MAX_VALUE;
         }
-        return shortPath(grid,dp,m-1,n-1);
-    }
-
-    public int shortPath(int[][] grid,int[][] dp,int i,int j){
-        if(i<0 || j<0){return Integer.MAX_VALUE;}
-        if(i==0 && j==0){return grid[i][j];}
+        if(i==0 && j==0){
+            return grid[i][j];
+        }
         if(dp[i][j]!=-1){return dp[i][j];}
-
-        int a=shortPath(grid,dp,i-1,j);
-        int b=shortPath(grid,dp,i,j-1);
-
-        int min=a>b?b:a;
-        
-        dp[i][j]=min+grid[i][j];
-        return min+grid[i][j];
+        int x=minPath(grid,i-1,j,dp);
+        int y=minPath(grid,i,j-1,dp);
+         dp[i][j]=Math.min(x,y)+grid[i][j];
+         return Math.min(x,y)+grid[i][j];
+    }
+    public int minPathSum(int[][] grid) {
+        int[][] dp=new int[grid.length][grid[0].length];
+        for(int[] arr:dp){Arrays.fill(arr,-1);}
+        return minPath(grid,grid.length-1,grid[0].length-1,dp);
     }
 }
