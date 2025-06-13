68063df99f385f5e1870a8974232531e4aa133e6
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-06-13 07:36:57 +0530

    Time: 1 ms (100.00%) | Memory: 42.8 MB (22.32%) - LeetSync

--- leetcode/1036-rotting-oranges/rotting-oranges.java
+++ leetcode/1036-rotting-oranges/rotting-oranges.java
@@ -1,74 +1,61 @@
 class Solution {
-     class Group{
-        int i;
-        int j;
+    class Pair{
+        int x;
+        int y;
         int time;
+        public Pair(int x, int y, int time){
+            this.x=x;
+            this.y=y;
+            this.time=time;
+        }
     }
     public int orangesRotting(int[][] grid) {
-       Queue<Group> q=new LinkedList<>();
-       for(int i=0;i<grid.length;i++) {
-           for(int j=0;j<grid[0].length;j++){
-               if(grid[i][j]==2){
-               Group group=new Group();
-               group.i=i;
-               group.j=j;
-               group.time=0;
-               q.add(group);
-               }
-           }
-       }
+        int r=grid.length;
+        int c=grid[0].length;
         int ans=0;
-       while(q.size()>0){
-           Group g=q.remove();
-           int i=g.i;
-           int j=g.j;
-           int time=g.time;
-           ans=time;
-
-           if(i-1>=0 && grid[i-1][j]==1){
-               grid[i-1][j]=2;
-               Group group=new Group();
-               group.i=i-1;
-               group.j=j;
-               group.time=time+1;
-               q.add(group);
-           }
-
-            if(i+1<grid.length && grid[i+1][j]==1){
-               grid[i+1][j]=2;
-               Group group=new Group();
-               group.i=i+1;
-               group.j=j;
-               group.time=time+1;
-               q.add(group);
-           }
-
-            if(j-1>=0 && grid[i][j-1]==1){
-               grid[i][j-1]=2;
-               Group group=new Group();
-               group.i=i;
-               group.j=j-1;
-               group.time=time+1;
-               q.add(group);
-           }
-
-            if(j+1<grid[0].length && grid[i][j+1]==1){
-               grid[i][j+1]=2;
-               Group group=new Group();
-               group.i=i;
-               group.j=j+1;
-               group.time=time+1;
-               q.add(group);
-           }
-       }
-
-       for(int i=0;i<grid.length;i++) {
-           for(int j=0;j<grid[0].length;j++){
-               if(grid[i][j]==1){
-               return -1;
-               }
-           }
-       }
-      return ans;  
+        Queue<Pair> q=new LinkedList<>();
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]==2){
+                  Pair p=new Pair(i,j,0);
+                  q.add(p);
+                }
+            }
+        }
+        while(q.size()>0){
+            Pair remove=q.remove();
+            int x=remove.x;
+            int y=remove.y;
+            int time=remove.time;
+            ans=time;
+            if(x-1>=0 && grid[x-1][y]==1){
+                grid[x-1][y]=2;
+                Pair pair=new Pair(x-1,y,time+1);
+                q.add(pair);
+            }
+            if(x+1<r && grid[x+1][y]==1){
+                grid[x+1][y]=2;
+                Pair pair=new Pair(x+1,y,time+1);
+                q.add(pair);
+            }
+            if(y-1>=0 && grid[x][y-1]==1){
+                grid[x][y-1]=2;
+                Pair pair=new Pair(x,y-1,time+1);
+                q.add(pair);
+            }
+            if(y+1<c && grid[x][y+1]==1){
+                grid[x][y+1]=2;
+                Pair pair=new Pair(x,y+1,time+1);
+                q.add(pair);
+            }
+        }
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                if(grid[i][j]==1){
+                  return -1;
+                }
+            }
+        }
+        return ans;
     }
 }
