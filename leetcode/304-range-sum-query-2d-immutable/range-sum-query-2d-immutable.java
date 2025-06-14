9d4022e7b785c50ea905a1d30a58e9828e78057f
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-06-14 18:01:54 +0530

    Time: 99 ms (76.01%) | Memory: 70.9 MB (39.33%) - LeetSync

--- leetcode/304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ leetcode/304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -1,41 +1,37 @@
 class NumMatrix {
- 
-   int[][] prefixSumArr;
+    int[][] prefixSum;
     public NumMatrix(int[][] matrix) {
-     int row=matrix.length;
-     int col=matrix[0].length;
-     prefixSumArr=new int[row][col];
-      
-     for(int i=0;i<row;i++){
-         for(int j=0;j<col;j++){
-             if(j==0){
-           prefixSumArr[i][j]=matrix[i][j];
-             }else{
-                 prefixSumArr[i][j]=prefixSumArr[i][j-1]+matrix[i][j];
-             }
-         }
-     }
-
-     for(int i=0;i<col;i++){
-         for(int j=1;j<row;j++){
-             prefixSumArr[j][i]+=prefixSumArr[j-1][i];
-         }
-     }
-
+        prefixSum=new int[matrix.length][matrix[0].length];
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(j==0){
+                   prefixSum[i][j]=matrix[i][j];
+                }else{
+                    prefixSum[i][j]=prefixSum[i][j-1]+matrix[i][j];
+                }   
+            }
+        }
+        for(int i=1;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                    prefixSum[i][j]+=prefixSum[i-1][j];  
+            }
+        }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int ans=prefixSumArr[row2][col2];
-        if(row1-1>=0){
-           ans=ans-prefixSumArr[row1-1][col2];
+        int r=prefixSum.length-1;
+        int c=prefixSum[0].length-1;
+
+        if(row1==0 && col1==0){
+            return prefixSum[row2][col2];
+        }else if(col1==0){
+           return prefixSum[row2][col2]-prefixSum[row1-1][col2];
+        }else if(row1==0){
+           return prefixSum[row2][col2]-prefixSum[row2][col1-1];
+        }else{
+            return prefixSum[row2][col2]-prefixSum[row2][col1-1]-prefixSum[row1-1][col2]
+            +prefixSum[row1-1][col1-1];
         }
-         if(col1-1>=0){
-           ans=ans-prefixSumArr[row2][col1-1];
-        }
-        if(row1-1>=0 && col1-1>=0){
-           ans=ans+prefixSumArr[row1-1][col1-1];
-        }
-        return ans;
     }
 }
 
