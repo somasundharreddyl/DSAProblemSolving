e9f90df5c46f2d05831db5e2d55ed93d6920f714
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-06-15 09:49:29 +0530

    Time: 3 ms (75.19%) | Memory: 50.7 MB (6.95%) - LeetSync

--- leetcode/229-majority-element-ii/majority-element-ii.java
+++ leetcode/229-majority-element-ii/majority-element-ii.java
@@ -1,53 +1,66 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        ArrayList<Integer> list=new ArrayList<>();
+        List<Integer> ans=new ArrayList<>();
+        int count1=0,count2=0;
         if(nums.length==1){
-            list.add(nums[0]);
-            return list;
+            ans.add(nums[0]);
+            return ans;
         }
-        int ans1=nums[0],ans2=-1,freq1=1,freq2=0;
-     for(int i=1;i<nums.length;i++){
+        if(nums.length==2){
+            if(nums[0]==nums[1]){
+            ans.add(nums[0]);
+            }else{
+                ans.add(nums[0]);
+                ans.add(nums[1]);
+            }
+            return ans;
+        }
+        int val1=nums[0],val2=nums[0],freq1=1,freq2=0;
 
-          if(nums[i]==ans1){
-             freq1++;
-         }else if(nums[i]==ans2){
-             freq2++;
-         }
-        else if(freq1==0){
-             ans1=nums[i];
-             freq1++;
-             continue;
-         }
-         else if(freq2==0){
-             ans2=nums[i];
-             freq2++;
-             continue;
-         }
-        else if(nums[i]!=ans1 && nums[i]!=ans2){
-             freq1--;
-             freq2--;
-         }
-         
-     }
-     int count1=0,count2=0;
-     if(freq1>0){
-         for(int i=0;i<nums.length;i++){
-             if(ans1==nums[i]){
-                 count1++;
-             }
-         }
-         if(count1>nums.length/3)
-         list.add(ans1);
-     }
-     if(freq2>0){
-         for(int i=0;i<nums.length;i++){
-             if(ans2==nums[i]){
-                 count2++;
-             }
-         }
-         if(count2>nums.length/3)
-         list.add(ans2);
-     }  
-     return list;
+        for(int i=1;i<nums.length;i++){
+          
+           if(val1==nums[i]){
+                  freq1++;
+                  continue;
+            }else{
+                if(freq1==0 && val2!=nums[i]){
+                    val1=nums[i];
+                    freq1++;
+                    continue;
+                }
+            }
+
+            if(val2==nums[i]){
+                  freq2++;
+                  continue;
+            }else{
+                if(freq2==0){
+                    val2=nums[i];
+                    freq2++;
+                    continue;
+                }  
+        }
+            freq1--;
+            freq2--;
+        }
+        if(val1==val2){
+            ans.add(val1);
+            return ans;
+        }
+        for(int i=0;i<nums.length;i++){
+            if(val1==nums[i]){
+                count1++;
+            }
+            if(val2==nums[i]){
+                count2++;
+            }
+        }
+        if(count1>nums.length/3){
+             ans.add(val1);
+        }
+        if(count2>nums.length/3){
+             ans.add(val2);
+        }
+        return ans;
     }
 }
