1b6e1c2bd1018c365316b8c9352f77fe31bb976c
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-06-15 11:08:43 +0530

    Time: 1 ms (23.02%) | Memory: 40.5 MB (56.10%) - LeetSync

--- leetcode/556-next-greater-element-iii/next-greater-element-iii.java
+++ leetcode/556-next-greater-element-iii/next-greater-element-iii.java
@@ -1,48 +1,43 @@
 class Solution {
     public int nextGreaterElement(int n) {
-
-        char[] charArr=(n+"").toCharArray();
-        int index=-1,idx2=-1;
-
-        for(int i=charArr.length-2;i>=0;i--){
-            if(charArr[i]<charArr[i+1]){
-                index=i;
+      char[] arr=(""+n).toCharArray();
+      int idx1=-1,idx2=-1;
+        for(int i=arr.length-1;i>0;i--){
+            if(arr[i]>arr[i-1]){
+                idx1=i-1;
                 break;
             }
         }
 
-        if(index==-1){
+        if(idx1!=-1){
+            int num=arr[idx1];
+            for(int i=arr.length-1;i>=idx1;i--){
+               if(arr[i]>num){
+                 idx2=i;
+                 break;
+               }
+            }
+            char temp=arr[idx1];
+            arr[idx1]=arr[idx2];
+            arr[idx2]=temp;
+
+            int start=idx1+1,end=arr.length-1;
+            while(start<end){
+                char temp1=arr[start];
+            arr[start]=arr[end];
+            arr[end]=temp1;
+              start++;
+              end--;
+            }
+          long l=Long.parseLong(new String(arr));
+          if(l>Integer.MAX_VALUE){
+            return -1;
+          }else{
+            return (int)l;
+          }
+        }else{
             return -1;
         }
-
-        for(int i=charArr.length-1;i>=0;i--){
-            if(charArr[i]>charArr[index]){
-                idx2=i;
-                break;
-            }
-        }
-
-        char temp=charArr[index];
-        charArr[index]=charArr[idx2];
-        charArr[idx2]=temp;
-
-        int start=index+1,end=charArr.length-1;
-
-        while(start<end){
-            char temp1=charArr[start];
-        charArr[start]=charArr[end];
-        charArr[end]=temp1;
-        start++;
-        end--;
-        }
-
-      long ans = Long.parseLong(new String(charArr));
-
-      if(ans>Integer.MAX_VALUE){
-          return -1;
-      }else{
-          return (int)ans;
-      }
 
     }
 }
