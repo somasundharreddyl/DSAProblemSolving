38ef9411e715d9819c85c8f25592eabe99fd091a
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-06-18 07:40:16 +0530

    Time: 1 ms (99.40%) | Memory: 46.2 MB (8.75%) - LeetSync

--- leetcode/260-single-number-iii/single-number-iii.java
+++ leetcode/260-single-number-iii/single-number-iii.java
@@ -1,34 +1,33 @@
 class Solution {
-    private boolean checkSetBit(int n,int i){
-        if((n & (1<<i))!=0){
-            return true;
-        }
-        return false;
-    }
     public int[] singleNumber(int[] nums) {
-        int shiftValue=0,val1=0,val2=0;
-        int[] ans=new int[2];
-        int xorValue=nums[0];
+
+        int val1=-1,val2=-1;
+        int val=nums[0];
+        int index=-1;
         for(int i=1;i<nums.length;i++){
-            xorValue=xorValue^(nums[i]);
+            val=val^nums[i];
         }
-
         for(int i=0;i<32;i++){
-          if(checkSetBit(xorValue,i)){
-            shiftValue=i;
-            break;
-          }
+            if((val&(1<<i))!=0){
+               index=i;
+               break;
+            }
         }
-
         for(int i=0;i<nums.length;i++){
-          if(checkSetBit(nums[i],shiftValue)){
-              val1=val1^(nums[i]);
-          }else{
-              val2=val2^(nums[i]);
-          }
+            if((nums[i] & (1<<index))!=0){
+               if(val1==-1){
+                val1=nums[i];
+               }else{
+                val1=val1^nums[i];
+               }
+            }else{
+               if(val2==-1){
+                val2=nums[i];
+               }else{
+                val2=val2^nums[i];
+               }
+            }
         }
-        ans[0]=val1;
-        ans[1]=val2;
-        return ans;
+           return new int[]{val1,val2};
     }
 }
