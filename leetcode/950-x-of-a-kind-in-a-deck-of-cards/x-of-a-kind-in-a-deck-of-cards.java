cfe19579b72b5b94096909fbd90434d1b5d51fb4
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-06-18 20:04:40 +0530

    Time: 11 ms (54.22%) | Memory: 45.4 MB (56.51%) - LeetSync

--- leetcode/950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.java
+++ leetcode/950-x-of-a-kind-in-a-deck-of-cards/x-of-a-kind-in-a-deck-of-cards.java
@@ -1,28 +1,21 @@
 class Solution {
-
-    private int gcd(int a, int b){
-        if(a==0){
-            return b;
-        }
-        return gcd(b%a,a);
-    }
     public boolean hasGroupsSizeX(int[] deck) {
         Map<Integer,Integer> map=new HashMap<>();
         for(int i=0;i<deck.length;i++){
-            if(map.containsKey(deck[i])==false){
-                map.put(deck[i],1);
-            }else{
-                int value=map.get(deck[i]);
-                map.put(deck[i],value+1);
-            }
+          map.put(deck[i],map.getOrDefault(deck[i],0)+1);
         }
-         int ans=0;
-        for(int key:map.keySet()){
-            ans=gcd(ans,map.get(key));
+        int ans=0;
+        for(int n:map.keySet()){
+           ans=gcd(map.get(n),ans);
         }
-        if(ans<=1){
+        if(ans!=1){
+            return true;
+        }else{
             return false;
         }
-        return true;
+    }
+    public int gcd(int a, int b){
+        if(a==0){return b;}
+        return gcd(b%a,a);
     }
 }
