4b4303c6ad6d26e4dda5e4722fa2bd5d2cdf4868
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-06-20 08:43:35 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (71.96%) - LeetSync

--- leetcode/154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
+++ leetcode/154-find-minimum-in-rotated-sorted-array-ii/find-minimum-in-rotated-sorted-array-ii.java
@@ -1,26 +1,23 @@
 class Solution {
     public int findMin(int[] nums) {
-        int low=0,high=nums.length-1,minIndex=-1;
+        int low=0,high=nums.length-1,ans=-1;
         while(low<=high){
             int mid=low+(high-low)/2;
-            if(nums[mid]==nums[low]&&nums[mid]==nums[high]){
+            if(low!=mid && mid!=high && nums[mid]==nums[low] && nums[high]==nums[mid]){
                 low++;
                 high--;
-                if(minIndex==-1 || nums[mid]<nums[minIndex]){
-                 minIndex=mid;  
-                }
             }else if(nums[mid]<=nums[high]){
-                if(minIndex==-1 || nums[mid]<nums[minIndex]){
-                 minIndex=mid;
+                if(ans==-1 || nums[mid]<nums[ans]){
+                    ans=mid;
                 }
                 high=mid-1;
             }else{
-                if(minIndex==-1 || nums[low]<nums[minIndex]){
-                    minIndex=low;
+                if(ans==-1 || nums[low]<nums[ans]){
+                    ans=low;
                 }
-                low=mid+1;
+                 low=mid+1;
             }
         }
-        return nums[minIndex];
+        return nums[ans];
     }
 }
