ea62eddca26cb3f60784c0a01ae26a2171ee0ee6
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-06-22 07:54:05 +0530

    Time: 32 ms (13.90%) | Memory: 46.1 MB (61.71%) - LeetSync

--- leetcode/227-basic-calculator-ii/basic-calculator-ii.java
+++ leetcode/227-basic-calculator-ii/basic-calculator-ii.java
@@ -1,59 +1,57 @@
 class Solution {
     public int calculate(String s) {
-        Stack<Integer> operand=new Stack<>();
-        Stack<Character> operator=new Stack<>();
-        int i=0;
-        while(i<s.length()){
+        Stack<Integer> operands=new Stack<>();
+        Stack<Character> operators=new Stack<>();
+        for(int i=0;i<s.length();i++){
             if(s.charAt(i)>='0' && s.charAt(i)<='9'){
                 int num=0;
-                while(i<s.length() && (s.charAt(i)>='0' && s.charAt(i)<='9')){
-                    num=num*10+(s.charAt(i)-'0');
+                while(i<s.length() && s.charAt(i)>='0' && s.charAt(i)<='9'){
+                    int a=s.charAt(i)-'0';
+                    num=(num*10)+a;
                     i++;
                 }
-                operand.push(num);
+                operands.push(num);
                 i--;
+            }else if(s.charAt(i)=='+' || s.charAt(i)=='-' || s.charAt(i)=='*' || s.charAt(i)=='/'){
+                
+                  while(operators.size()>0 && precedence(s.charAt(i))<=precedence(operators.peek())){
+                    int val2=operands.pop();
+                    int val1=operands.pop();
+                    char c=operators.pop();
+                    int cal=calculate(val1,val2,c);
+                    operands.push(cal);
+                  }
+            
+                operators.push(s.charAt(i));
             }
-            else if(s.charAt(i)=='+' || s.charAt(i)=='-' || s.charAt(i)=='*' || s.charAt(i)=='/'){
-                while(operator.size()>0 && precedence(s.charAt(i))<=precedence(operator.peek())){
-                   char c=operator.pop();
-                   int val2=operand.pop();
-                   int val1=operand.pop();
-                   int res=calc(val1,val2,c);
-                   operand.push(res);
-                }
-                operator.push(s.charAt(i));
-            }
-            i++;
         }
-        while(operator.size()>0){
-             char c=operator.pop();
-                   int val2=operand.pop();
-                   int val1=operand.pop();
-                   int res=calc(val1,val2,c);
-                   operand.push(res);
+        while(operators.size()>0){
+                    int val2=operands.pop();
+                    int val1=operands.pop();
+                    char c=operators.pop();
+                    int cal=calculate(val1,val2,c);
+                    operands.push(cal);
         }
-        return operand.pop();
+        return operands.pop();
     }
-
-    private int precedence(char c){
-        if(c=='*' || c=='/'){
+    public int precedence(char ch){
+        if(ch=='+' || ch=='-'){
             return 1;
-        }else if(c=='+' || c=='-'){
-            return 0;
+        }else if(ch=='*' || ch=='/'){
+            return 2;
+        }else{
+            return 3;
         }
-        return -1;
     }
-
-    private int calc(int num1,int num2, char ch){
-        if(ch=='*'){
-            return num1*num2;
-        }else if(ch=='/'){
-            return num1/num2;
-        }else if(ch=='+'){
-            return num1+num2;
-        }else if(ch=='-'){
-            return num1-num2;
+    public int calculate(int a,int b,char c){
+        if(c=='+'){
+            return a+b;
+        }else if(c=='-'){
+            return a-b;
+        }else if(c=='*'){
+            return a*b;
+        }else{
+            return a/b;
         }
-        return -1;
     }
 }
