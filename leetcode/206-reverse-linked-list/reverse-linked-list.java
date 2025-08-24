514c7f126154aff8dae0fd1415319435c3334f96
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-08-24 14:09:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.6 MB (51.30%) - LeetSync

--- leetcode/206-reverse-linked-list/reverse-linked-list.java
+++ leetcode/206-reverse-linked-list/reverse-linked-list.java
@@ -10,15 +10,14 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        ListNode prev=null;
-        ListNode curr=head;
+        ListNode prev=null,curr=head;
         while(curr!=null){
-         ListNode nextNode=curr.next;
-         curr.next=prev;
-         prev=curr;
-         curr=nextNode;
+            ListNode temp=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=temp;
         }
         head=prev;
-      return head;
+        return head;
     }
 }
