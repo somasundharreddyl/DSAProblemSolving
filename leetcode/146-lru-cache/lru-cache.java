319b379b03ee81966387be7dbc2b2ac9c0185192
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-10-01 12:50:25 +0530

    Time: 45 ms (65.20%) | Memory: 114 MB (53.96%) - LeetSync

--- leetcode/146-lru-cache/lru-cache.java
+++ leetcode/146-lru-cache/lru-cache.java
@@ -1,70 +1,73 @@
 class LRUCache {
+
+    int size;
+    Map<Integer,Node> hm;
+    Node startNode=new Node();
+    Node endNode=new Node();
+
+    public LRUCache(int capacity) {
+        size=capacity;
+        hm=new HashMap<>();
+        startNode.next=endNode;
+        endNode.prev=startNode;
+    }
+    
+    public int get(int key) {
+        if(hm.containsKey(key)){
+          Node remNode=delete(hm.get(key));
+          add(remNode,endNode);
+          return remNode.val;
+        }
+        return -1;
+    }
+    
+    public void put(int key, int value) {
+        if(hm.containsKey(key)){
+           Node remNode=delete(hm.get(key));
+           remNode.val=value;
+           Node addNode=add(remNode,endNode);
+           hm.put(key,addNode);
+        }
+        else{
+            if(hm.size()>=size){
+                Node remNode=delete(startNode.next);
+                hm.remove(remNode.key);
+            }
+           Node addNode=add(new Node(key,value),endNode);
+           hm.put(key,addNode);
+        }
+    }
+
+    public Node add(Node node,Node endNode){
+         Node prevM1=endNode.prev;
+         prevM1.next=node;
+         node.next=endNode;
+         node.prev=prevM1;
+         endNode.prev=node;
+         return node;
+    }
+
+    public Node delete(Node node){
+         Node prevm1=node.prev;
+         Node nextp1=node.next;
+         prevm1.next=nextp1;
+         nextp1.prev=prevm1;
+         node.next=null;
+         node.prev=null;
+         return node;
+
+    }
+
     class Node{
         int key;
         int val;
         Node next;
         Node prev;
-        public Node(){
+        Node(int key,int val){
+            this.key=key;
+            this.val=val;
         }
-        public Node(int key,int val){
-           this.key=key;
-           this.val=val;
-        }
-    }
-    Map<Integer,Node> map;
-    Node head=new Node();
-    Node tail=new Node();
-    int size;
-    
-    public LRUCache(int capacity) {
-        map=new HashMap<>();
-        head.next=tail;
-        tail.prev=head;
-        size=capacity;
-    }
-    
-    public int get(int key) {
-        if(map.containsKey(key)){
-         Node temp=deleteNode(head,map.get(key));
-           addNode(tail,temp);
-            return map.get(key).val;
-        }else{
-            return -1;
-        }
-    }
-    
-    public void put(int key, int value) {
-          if(map.containsKey(key)){
-            Node rem=deleteNode(head,map.get(key));
-            rem.val=value;
-            map.put(key,rem);
-            addNode(tail,rem);
-          }else{     
-        if(map.size()>=size){
-         Node rem=deleteNode(head,head.next);
-         map.remove(rem.key);
-        }
-        Node node=new Node(key,value);
-          map.put(key,node);
-        addNode(tail,node);
-          }
-    }
-    public Node deleteNode(Node head,Node temp){
-        Node cm1=temp.prev;
-        Node cp1=temp.next;
-        cm1.next=cp1;
-        cp1.prev=cm1;
-        temp.next=null;
-        temp.prev=null;
-        return temp;
-    }
-    
-    public void addNode(Node tail,Node temp){
-        Node cm1=tail.prev;
-        cm1.next=temp;
-        temp.next=tail;
-        tail.prev=temp;
-        temp.prev=cm1;
+        Node(){}
     }
 }
 
