9f840dde560a23fb803f2008df8ba81ad4c718fa
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-10-01 13:55:36 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (30.92%) - LeetSync

--- leetcode/138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
+++ leetcode/138-copy-list-with-random-pointer/copy-list-with-random-pointer.java
@@ -18,32 +18,33 @@ class Solution {
         if(head==null){return head;}
         Node temp=head;
         while(temp!=null){
-            Node n=new Node(temp.val);
             Node tempp1=temp.next;
-            temp.next=n;
-            n.next=tempp1;
+            Node copyNode=new Node(temp.val);
+            temp.next=copyNode;
+            copyNode.next=tempp1;
             temp=tempp1;
         }
-        temp=head;
-        while(temp!=null){
-            if(temp.random!=null){
-         temp.next.random=temp.random.next;
-            }
-            temp=temp.next.next;
+        Node tempNode=head;
+        while(tempNode!=null ){
+             Node rnd=tempNode.random;
+             if(rnd!=null){
+             tempNode.next.random=rnd.next;  
+             }
+             tempNode=tempNode.next.next;
         }
-        Node h1=head;
-        Node h2=head.next;
+        Node head1=head;
         Node ans=head.next;
-        while(h1!=null){
-            Node h1p1=h1.next.next;
-            Node h2p1=null;
-            if(h2.next!=null){
-                h2p1=h2.next.next;
-            }
-            h1.next=h1p1;
-            h2.next=h2p1;
-            h1=h1.next;
-            h2=h2.next;
+        Node head2=head.next;
+        while(head1!=null){
+        Node head1p1=head1.next.next;
+        Node head2p2=null;
+        if(head2.next!=null){
+        head2p2=head2.next.next;
+        }
+           head1.next=head1p1;
+           head2.next=head2p2;
+           head1=head1p1;
+           head2=head2p2;
         }
         return ans;
     }
