7f12df1e5dacdeab99de3062c99fb3bb11c85c7b
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-10-09 07:22:50 +0530

    Time: 0 ms (100.00%) | Memory: 45.8 MB (12.68%) - LeetSync

--- leetcode/783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
+++ leetcode/783-search-in-a-binary-search-tree/search-in-a-binary-search-tree.java
@@ -15,19 +15,17 @@
  */
 class Solution {
     public TreeNode searchBST(TreeNode root, int val) {
-        if(root==null){return root;}
-        Queue<TreeNode> q=new LinkedList<>();
-        q.add(root);
-        while(q.size()>0){
-          TreeNode node=q.remove();
-          if(node==null){return null;}
-          if(node.val==val){
-            return node;
-          }else if(val>node.val){
-            q.add(node.right);
-          }else{
-            q.add(node.left);
-          }
+        TreeNode temp=root;
+        while(temp!=null){
+            if(temp.val==val){
+                return temp;
+            }
+            else if(val<temp.val){
+                temp=temp.left;
+            }
+            else if(val>temp.val){
+                temp=temp.right;
+            }
         }
         return null;
     }
