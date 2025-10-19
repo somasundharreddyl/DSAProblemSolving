80f3e07a25dd7c67334c8c8996c0ffdae6a77f58
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-10-19 14:34:08 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (34.19%) - LeetSync

--- leetcode/116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
+++ leetcode/116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.java
@@ -27,25 +27,20 @@ class Solution {
             return root;
         }
         Node temp=root;
-        Queue<Node> q = new LinkedList<>();
-        q.add(temp);
-        while(q.size()>0){
-          int size=q.size();
-          for(int i=0;i<size;i++){
-            Node rem=q.remove();
-            if(i!=size-1){
-             rem.next=q.peek();
-            }else{
-             rem.next=null;
-            }
-            if(rem.left!=null){
-                q.add(rem.left);
-            }
-            if(rem.right!=null){
-                q.add(rem.right);
-            }
-          }
+        while(temp!=null){
+            Node temp1=temp;
+           while(temp1!=null && temp1.left!=null){
+             temp1.left.next=temp1.right;
+             if(temp1.next!=null){
+               temp1.right.next=temp1.next.left;
+             }else{
+               temp1.right.next=null;
+             } 
+           temp1=temp1.next; 
+           } 
+           temp=temp.left;
         }
+        
         return root;
     }
 }
