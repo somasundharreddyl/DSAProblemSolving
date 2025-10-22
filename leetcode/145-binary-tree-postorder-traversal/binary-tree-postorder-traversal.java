5c9f0da2af42aaeebf1d221602deae7cb035eb95
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-10-22 10:08:55 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (83.97%) - LeetSync

--- leetcode/145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
+++ leetcode/145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.java
@@ -15,17 +15,35 @@
  */
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
-      List<Integer> ans=new ArrayList<>();
+        List<Integer> ans=new ArrayList<>();
         TreeNode temp=root;
-        traverseNode(temp,ans);
-        return ans;
-    }
-    public void traverseNode(TreeNode root,List<Integer> ans){
-            if(root==null){
-                return;
+        while(temp!=null){
+            if(temp.right==null){
+                ans.add(temp.val); //1,
+                temp=temp.left;
+            }else{
+                TreeNode temp1=temp.right;
+                while(temp1.left!=null && temp1.left!=temp){
+                    temp1=temp1.left;
+                }
+                if(temp1.left==null){
+                    temp1.left=temp;
+                    ans.add(temp.val);
+                    temp=temp.right;
+                }else{
+                    temp1.left=null;
+                    temp=temp.left;
+                }
             }
-            traverseNode(root.left,ans);
-            traverseNode(root.right,ans);
-            ans.add(root.val);
+        }
+        int s=0,e=ans.size()-1;
+        while(s<=e){
+            int t=ans.get(s);
+            ans.set(s,ans.get(e));
+            ans.set(e,t);
+            s++;
+            e--;
+        }
+        return ans;
     }
 }
