d7b95e375375b82fe9a64f13106c67d5e383a08f
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-10-28 15:12:16 +0530

    Time: 80 ms (48.19%) | Memory: 60.5 MB (65.06%) - LeetSync

--- leetcode/1043-grid-illumination/grid-illumination.java
+++ leetcode/1043-grid-illumination/grid-illumination.java
@@ -1,39 +1,40 @@
 class Solution {
     public int[] gridIllumination(int n, int[][] lamps, int[][] queries) {
+        Map<Integer,Integer> hmRow=new HashMap<>();
+        Map<Integer,Integer> hmCol=new HashMap<>();
+        Map<Integer,Integer> hmRminusC=new HashMap<>();
+        Map<Integer,Integer> hmRplusC=new HashMap<>();
+        Map<Integer,Integer> hmBox=new HashMap<>();
+        int[][] directions={{-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1},{0,0}};
         int[] ans=new int[queries.length];
-        int[][] dir=new int[][]{{-1,-1},{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1},{0,0}};
-        Map<Integer,Integer> rows=new HashMap<>();
-        Map<Integer,Integer> cols=new HashMap<>();
-        Map<Integer,Integer> topLeftD=new HashMap<>();
-        Map<Integer,Integer> topRightD=new HashMap<>();
-        Map<Integer,Integer> lampsMap=new HashMap<>();
         for(int i=0;i<lamps.length;i++){
             int x=lamps[i][0];
             int y=lamps[i][1];
-            rows.put(x,rows.getOrDefault(x,0)+1);
-            cols.put(y,cols.getOrDefault(y,0)+1);
-            topLeftD.put(x-y,topLeftD.getOrDefault(x-y,0)+1);
-            topRightD.put(x+y,topRightD.getOrDefault(x+y,0)+1);
-            lampsMap.put(x*n+y,lampsMap.getOrDefault(x*n+y,0)+1);   
+            hmRow.put(x,hmRow.getOrDefault(x,0)+1);
+            hmCol.put(y,hmCol.getOrDefault(y,0)+1);
+            hmRminusC.put(x-y,hmRminusC.getOrDefault(x-y,0)+1);
+            hmRplusC.put(x+y,hmRplusC.getOrDefault(x+y,0)+1);
+            hmBox.put(x*n+y,hmBox.getOrDefault(x*n+y,0)+1);
         }
+
         for(int i=0;i<queries.length;i++){
-            int x1=queries[i][0];
-            int y1=queries[i][1];
-             if(rows.getOrDefault(x1,0)>0 || cols.getOrDefault(y1,0)>0 || topLeftD.getOrDefault(x1-y1,0)>0 || 
-topRightD.getOrDefault(x1+y1,0)>0){
+            int x=queries[i][0];
+            int y=queries[i][1];
+            if(hmRow.getOrDefault(x,0)>0 || hmCol.getOrDefault(y,0)>0 || hmRminusC.getOrDefault(x-y,0)>0
+            || hmRplusC.getOrDefault(x+y,0)>0 || hmBox.getOrDefault(x*n+y,0)>0){
                 ans[i]=1;
             }
-            for(int d=0;d<dir.length;d++){
-               int x=x1+dir[d][0];
-               int y=y1+dir[d][1];
-               if(x>=0 && x<n && y>=0 && y<n && lampsMap.containsKey(x*n+y)){
-               int times=lampsMap.get(x*n+y);
-            rows.put(x,rows.getOrDefault(x,0)-times);
-            cols.put(y,cols.getOrDefault(y,0)-times);
-            topLeftD.put(x-y,topLeftD.getOrDefault(x-y,0)-times);
-            topRightD.put(x+y,topRightD.getOrDefault(x+y,0)-times);
-            lampsMap.remove(x*n+y);
-               }
+            for(int j=0;j<directions.length;j++){
+                int x1=x+directions[j][0];
+                int y1=y+directions[j][1];
+                if(x1>=0 && x1<n && y1>=0 && y1<n && hmBox.containsKey(x1*n+y1)){
+                      int count=hmBox.get(x1*n+y1);
+                      hmRow.put(x1,hmRow.getOrDefault(x1,0)-count);
+                      hmCol.put(y1,hmCol.getOrDefault(y1,0)-count);
+                      hmRminusC.put(x1-y1,hmRminusC.getOrDefault(x1-y1,0)-count);
+                      hmRplusC.put(x1+y1,hmRplusC.getOrDefault(x1+y1,0)-count);
+                      hmBox.remove(x1*n+y1);    
+                }
             }
         }
         return ans;
