030e98400f5e9043aad6da00a91d98f2b182c7d1
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-10-29 14:32:25 +0530

    Time: 1098 ms (10.22%) | Memory: 46.2 MB (84.36%) - LeetSync

--- leetcode/560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ leetcode/560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,25 +1,15 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-         int count=0;
-        if(nums.length==1 && nums[0]!=k){
-             return count;
+        int ans=0;
+        for(int s=0;s<nums.length;s++){
+            int count=0;
+           for(int e=s;e<nums.length;e++){
+            count+=nums[e];
+            if(count==k){
+              ans++;
+            } 
+           }
         }
-        Map<Integer,Integer> hm=new HashMap<>();
-        int[] pnums=new int[nums.length];
-        pnums[0]=nums[0];
-        hm.put(0,1);
-        for(int i=1;i<nums.length;i++){
-        pnums[i]=pnums[i-1]+nums[i];
-        }
-        for(int i=0;i<pnums.length;i++){
-           if(hm.containsKey(pnums[i]-k)){
-               count+=hm.get(pnums[i]-k);
-            }
-            hm.put(pnums[i],hm.getOrDefault(pnums[i],0)+1);
-        }
-       return count; 
+        return ans;
     }
 }
-//[1,2,3]=>2
-//[1,3,6]=>3
-//[-1,-2,-1]
