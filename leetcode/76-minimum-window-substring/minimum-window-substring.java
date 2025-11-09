bab81459c8c73138c809bf5cc4365d3b1c14757a
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-11-09 12:41:33 +0530

    Time: 20 ms (42.73%) | Memory: 48.8 MB (5.17%) - LeetSync

--- leetcode/76-minimum-window-substring/minimum-window-substring.java
+++ leetcode/76-minimum-window-substring/minimum-window-substring.java
@@ -1,60 +1,55 @@
 class Solution {
     public String minWindow(String s, String t) {
-         if(t.length() > s.length()) return ""; 
-        HashMap<Character,Integer> smap = new HashMap<>(); 
-        HashMap<Character,Integer> tmap = new HashMap<>(); 
-        int matchcount = 0; 
-         
-        for(int i = 0; i<t.length(); i++){ 
-            char ch = t.charAt(i); 
-            tmap.put(ch , tmap.getOrDefault(ch,0)+1); 
-        } 
-        int anslen = Integer.MAX_VALUE; 
-        int anssp = 0,ansep = 0; 
-        int sp = 0; 
-        int ep = 0; 
-        while(ep<s.length()){ 
-            if(matchcount == t.length()){ 
-                //release 
-                char ch1 = s.charAt(sp); 
-                if(ep-sp<anslen){ 
-                    anslen = ep-sp; 
-                    anssp = sp; 
-                    ansep = ep; 
-                } 
-                smap.put(ch1, smap.get(ch1)-1); 
-                if(smap.get(ch1) < tmap.getOrDefault(ch1,0)){ 
-                    matchcount--; 
-                } 
-                sp++; 
-            } else { 
-                //acquire 
-                char ch2 = s.charAt(ep); 
-                smap.put(ch2, smap.getOrDefault(ch2,0)+1); 
-                if(smap.get(ch2) <= tmap.getOrDefault(ch2,0)){ 
-                    matchcount++; 
-                } 
-                 
-                ep++; 
-            } 
+        String ans="";
+        if(s.length()<t.length()){
+            return ans;
+        }
+        Map<Character,Integer> sMap=new HashMap<>();
+        Map<Character,Integer> tMap=new HashMap<>();
+        for(char ch:t.toCharArray()){
+            tMap.put(ch,tMap.getOrDefault(ch,0)+1);
+        }
+        int start=0,end=0,ansStartIdx=0,ansEndIdx=0,matchCount=0,anslen=Integer.MAX_VALUE;
+        
+            while(end<s.length()){
+            if(matchCount==t.length()){
+                if(ansStartIdx==0 && ansEndIdx==0){
+                   ansStartIdx=start;
+                   ansEndIdx=end;
+                }else if(ansEndIdx-ansStartIdx>end-start){
+                   ansStartIdx=start;
+                   ansEndIdx=end;
+                }
+              sMap.put(s.charAt(start),sMap.get(s.charAt(start))-1);  
+              if(sMap.get(s.charAt(start))<tMap.getOrDefault(s.charAt(start),0)){
+                    matchCount--;                                            
+              }
               
-        } 
-         
-        while(matchcount==t.length()){ 
-            char ch1 = s.charAt(sp); 
-                if(ep-sp<anslen){ 
-                    anslen = ep-sp; 
-                    anssp = sp; 
-                    ansep = ep; 
-                } 
-                smap.put(ch1, smap.get(ch1)-1); 
-                if(smap.get(ch1) < tmap.getOrDefault(ch1,0)){ 
-                    matchcount--; 
-                } 
-                sp++; 
-             
-        } 
-         
-        return s.substring(anssp , ansep); 
-    } 
-} 
+              start++;
+            }
+            else{
+                sMap.put(s.charAt(end),sMap.getOrDefault(s.charAt(end),0)+1);
+                if(sMap.get(s.charAt(end))<=tMap.getOrDefault(s.charAt(end),0)){
+                    matchCount++;
+                }
+                end++;
+            }
+        }
+        while(matchCount==t.length()){
+           if(ansStartIdx==0 && ansEndIdx==0){
+                   ansStartIdx=start;
+                   ansEndIdx=end;
+                }else if(ansEndIdx-ansStartIdx>end-start){
+                   ansStartIdx=start;
+                   ansEndIdx=end;
+                }
+                sMap.put(s.charAt(start),sMap.get(s.charAt(start))-1);
+              if(sMap.get(s.charAt(start))<tMap.getOrDefault(s.charAt(start),0)){
+                    matchCount--;                                            
+              }
+              
+              start++; 
+        }
+        return s.substring(ansStartIdx,ansEndIdx);
+    }
+}
