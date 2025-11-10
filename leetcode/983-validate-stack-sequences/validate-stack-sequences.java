6f990ef2e6dbdecfbaaeb18bc9d8464767986ff9
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-11-10 09:28:53 +0530

    Time: 2 ms (86.35%) | Memory: 46.2 MB (5.14%) - LeetSync

--- leetcode/983-validate-stack-sequences/validate-stack-sequences.java
+++ leetcode/983-validate-stack-sequences/validate-stack-sequences.java
@@ -1,40 +1,30 @@
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
-        Stack<Integer> st=new Stack<>();
-        int i=0;
-int j=0;
+        Stack<Integer> stack=new Stack<>();
+        int i=0,j=0;
+        while(i<pushed.length){
+            if(stack.size()==0){
+                stack.push(pushed[i]);
+                i++;
+                continue;
+            }
+            if(popped[j]==stack.peek()){
+              stack.pop();
+              j++;
+            }else{
+                stack.push(pushed[i]);
+                i++;
+            }
+        }
 
-
-while(i<pushed.length && j<popped.length){
-if(st.size()==0){
-st.push(pushed[i]);
-i++;
-continue;
-}
-
-
-if(popped[j] == st.peek()){
-st.pop();
-j++;
-}else{
-st.push(pushed[i]);
-i++;
-}
-}
-
-
-while(j < popped.length && popped[j] == st.peek()){
-st.pop();
-j++;
-}
-
-
-if(st.size()>0){
-return false;
-}else{
-return true;
-}
-
-        
+        while(stack.size()>0){
+            if(popped[j]==stack.peek()){
+              stack.pop();
+              j++;
+            }else{
+                return false;
+            }
+        }
+        return true;
     }
 }
