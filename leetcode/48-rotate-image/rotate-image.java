99e2b1604c593651f0b4e03ebf78887ba05c13c8
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-11-24 07:16:59 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (26.10%) - LeetSync

--- leetcode/48-rotate-image/rotate-image.java
+++ leetcode/48-rotate-image/rotate-image.java
@@ -1,22 +1,30 @@
 class Solution {
     public void rotate(int[][] matrix) {
+        transpose(matrix);
+        reverse(matrix);
+    }
+    public void transpose(int[][] matrix){
         for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<i;j++){
+            for(int j=i;j<matrix[i].length;j++){
+                if(i!=j){
                 int temp=matrix[i][j];
                 matrix[i][j]=matrix[j][i];
                 matrix[j][i]=temp;
+                }
             }
         }
-        for(int i=0;i<matrix.length;i++){
-            int start=0,end=matrix[0].length-1;
-            while(start<end){
-             int temp=matrix[i][start];
-                matrix[i][start]=matrix[i][end];
-                matrix[i][end]=temp;
-                start++;
-                end--;
-            }
+    }
+    public void reverse(int[][] matrix){
+       
+       for(int i=0;i<matrix.length;i++){
+        int start=0,end=matrix[0].length-1;
+        while(start<end){
+            int temp=matrix[i][start];
+            matrix[i][start]=matrix[i][end];
+            matrix[i][end]=temp;
+            start++;
+            end--;
         }
-
+       }
     }
 }
