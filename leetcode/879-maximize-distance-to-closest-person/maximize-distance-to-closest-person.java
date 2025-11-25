136d378870edabdc1f1c708071f6762bab2f7693
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-11-25 15:39:53 +0530

    Time: 3 ms (47.14%) | Memory: 47.6 MB (30.50%) - LeetSync

--- leetcode/879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.java
+++ leetcode/879-maximize-distance-to-closest-person/maximize-distance-to-closest-person.java
@@ -1,26 +1,25 @@
 class Solution {
     public int maxDistToClosest(int[] seats) {
-        int start=-1,end=-1,maxIndex=-1,maxDist=-1;
+        int maxDist=1;    //2
+        int occupyIdx=-1; //1
         for(int i=0;i<seats.length;i++){
-            if(i==0 && (seats[i]==0 || seats[i]==1)){
-                start=i;
-            }else if(seats[i]==1 || (i==seats.length-1 && (seats[i]==0 || seats[i]==1))){
-                if((i==seats.length-1 && seats[i]==0) || (start==0 && seats[start]==0)){
-                 int dist=(i-start);
-                 int index=i; 
-                 if(dist>maxDist){
-                    maxDist=dist;
-                    maxIndex=index;
-                 }
+            if(occupyIdx==-1 ){
+                occupyIdx=i;
+                continue;
+            }
+            if(seats[i]==1){
+                if(occupyIdx==0 && seats[occupyIdx]==0){
+                  int dist=(i-occupyIdx);
+                  maxDist=maxDist>dist?maxDist:dist;  
                 }else{
-                 int dist=(i-start)/2;
-                 int index=(i+start)/2;
-                 if(dist>maxDist){
-                    maxDist=dist;
-                    maxIndex=index;
-                 }
+                  int dist=(i-occupyIdx)/2;
+                maxDist=maxDist>dist?maxDist:dist; 
                 }
-                 start=i;
+                occupyIdx=i;
+            }
+            if(i==seats.length-1 && seats[i]==0){
+                int dist=(i-occupyIdx);
+                maxDist=maxDist>dist?maxDist:dist;
             }
         }
         return maxDist;
