306491cbf0695a527987b016d6f4ff8413846822
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-11-25 15:03:17 +0530

    Time: 1 ms (25.28%) | Memory: 41.7 MB (33.83%) - LeetSync

--- leetcode/556-next-greater-element-iii/next-greater-element-iii.java
+++ leetcode/556-next-greater-element-iii/next-greater-element-iii.java
@@ -1,43 +1,45 @@
 class Solution {
     public int nextGreaterElement(int n) {
-      char[] arr=(""+n).toCharArray();
-      int idx1=-1,idx2=-1;
-        for(int i=arr.length-1;i>0;i--){
-            if(arr[i]>arr[i-1]){
-                idx1=i-1;
+
+        char[] chArr = (n+"").toCharArray();
+        if(chArr.length==0 || chArr.length==1){
+            return -1;
+        }
+        int idx=chArr.length-1;       //1
+        for(int i=chArr.length-2;i>=0;i--){
+             if(chArr[idx]>chArr[i]){
+                int k=idx;  //1
+                while(k<chArr.length && chArr[k]>chArr[i]){
+                    k++;          
+                }
+                k--;
+                char temp=chArr[i];  
+                chArr[i]=chArr[k];  
+                chArr[k]=temp;       
+                int start=idx,end=chArr.length-1;
+                reverseArray(chArr,start,end);
                 break;
-            }
-        }
-
-        if(idx1!=-1){
-            int num=arr[idx1];
-            for(int i=arr.length-1;i>=idx1;i--){
-               if(arr[i]>num){
-                 idx2=i;
-                 break;
-               }
-            }
-            char temp=arr[idx1];
-            arr[idx1]=arr[idx2];
-            arr[idx2]=temp;
-
-            int start=idx1+1,end=arr.length-1;
-            while(start<end){
-                char temp1=arr[start];
-            arr[start]=arr[end];
-            arr[end]=temp1;
-              start++;
-              end--;
-            }
-          long l=Long.parseLong(new String(arr));
-          if(l>Integer.MAX_VALUE){
+             }  
+             idx--;
+           if(i==0){
             return -1;
-          }else{
-            return (int)l;
-          }
-        }else{
-            return -1;
+           }   
         }
-
+         long ans= Long.parseLong(new String(chArr));
+         if(ans>Integer.MAX_VALUE){
+            return -1;
+         }else{
+            return (int)ans;
+         }
     }
+    public void reverseArray(char[] ch,int start,int end){
+          while(start<end){
+            char temp=ch[start];
+            ch[start]=ch[end];
+            ch[end]=temp;
+            start++;
+            end--;
+          }
+    }
+
 }
