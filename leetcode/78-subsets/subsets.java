bdaa945fef1f7fcb6fe41f16026db5f7fd9859e5
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-11-29 08:38:19 +0530

    Time: 1 ms (65.76%) | Memory: 44.1 MB (30.31%) - LeetSync

--- leetcode/78-subsets/subsets.java
+++ leetcode/78-subsets/subsets.java
@@ -1,22 +1,22 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans=new ArrayList<>();
-        int total=1<<nums.length;
+        int total=(1<<nums.length);
         for(int i=0;i<total;i++){
-            List<Integer> list=new ArrayList<>();
+            List<Integer> temp=new ArrayList<>();
             for(int j=0;j<nums.length;j++){
                 if(checkBit(i,j)){
-                    list.add(nums[j]);
+                    temp.add(nums[j]);
                 }
             }
-            ans.add(list);
+            ans.add(temp);
         }
         return ans;
     }
 
-    public boolean checkBit(int n, int j){
+    public boolean checkBit(int n,int j){
         if((n&(1<<j))!=0){
-            return true;
+           return true;
         }
         return false;
     }
