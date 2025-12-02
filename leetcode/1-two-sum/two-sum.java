1bd4810ba9b82d99459f015d74163076c9e4640d
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-12-02 07:22:05 +0530

    Time: 7 ms (46.06%) | Memory: 47.5 MB (7.19%) - LeetSync

--- leetcode/1-two-sum/two-sum.java
+++ leetcode/1-two-sum/two-sum.java
@@ -1,37 +1,35 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-      int[] arr=new int[nums.length];
-      for(int k=0;k<nums.length;k++){
-          arr[k]=nums[k];
-      }
-      Arrays.sort(nums);
-      int i=0,j=nums.length-1;
-      int[] ans=new int[]{-1,-1};
-      while(i<j){
-      if(nums[i]+nums[j]==target){
-          ans[0]=i;
-          ans[1]=j;
-          break;
-      }else if(nums[i]+nums[j]>target){
-          j--;
-      }else{
-          i++;
-      }
-      }
-      if(ans[0]!=-1 && ans[1]!=-1){
-      for(int k=0;k<nums.length;k++){
-          if(arr[k]==nums[ans[0]]){
-              ans[0]=k;
-              break;
-          }
-      } 
-      for(int k=nums.length-1;k>=0;k--){
-          if(arr[k]==nums[ans[1]]){
-              ans[1]=k;
-              break;
-          }
-      }
-      } 
-      return ans;
+        int[] arr=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            arr[i]=nums[i];
+        }
+        Arrays.sort(arr);
+        int i=0,j=arr.length-1;
+        int[] ans=new int[]{-1,-1};
+        while(i<j){
+           if(arr[i]+arr[j]==target){
+             ans[0]=i;
+             ans[1]=j;
+             break;
+           }else if(arr[i]+arr[j]>target){
+             j--;
+           }else{
+            i++;
+           }
+        }
+        for(int k=0;k<nums.length;k++){
+            if(nums[k]==arr[ans[0]]){
+                ans[0]=k;
+                break;
+            }
+        }
+        for(int k=0;k<nums.length;k++){
+            if(ans[0]!=k && nums[k]==arr[ans[1]]){
+                ans[1]=k;
+                break;
+            }
+        }
+        return ans;
     }
 }
