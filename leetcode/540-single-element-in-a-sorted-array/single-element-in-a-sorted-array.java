4d5dfe2046c6facecf73c6a60ec4454d4658601a
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-12-08 07:44:39 +0530

    Time: 0 ms (100.00%) | Memory: 53 MB (18.46%) - LeetSync

--- leetcode/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ leetcode/540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -3,24 +3,19 @@ class Solution {
         if(nums.length==1){
             return nums[0];
         }
-        if(nums[0]!=nums[1]){
-          return nums[0];
-        }
-        if(nums[nums.length-1]!=nums[nums.length-2]){
-            return nums[nums.length-1];
-        }
-        int start=2,end=nums.length-3;
+        int start=0,end=nums.length-1;
         while(start<=end){
-            int mid=start+(end-start)/2;
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]){
-                return nums[mid];
-            }
- else if((mid%2==0 && nums[mid]==nums[mid-1]) ||( mid%2==1 && nums[mid]==nums[mid+1] ))          {
-               end=mid-1;
-            }else{
-                start=mid+1;
-            }
-        
+           int mid=start+(end-start)/2;
+           if((mid!=0 && mid!=nums.length-1 && 
+           nums[mid]!=nums[mid+1] && nums[mid]!=nums[mid-1]) || (mid==0 && nums[mid]!=nums[mid+1]) ||
+           (mid==nums.length-1 && nums[mid]!=nums[mid-1])){
+             return nums[mid];
+           }else if((mid!=nums.length-1 && mid%2==0 && nums[mid]!=nums[mid+1]) 
+           || (mid!=0 && mid%2==1 && nums[mid]!=nums[mid-1])){
+            end=mid-1;
+           }else{
+            start=mid+1;
+           }
         }
         return -1;
     }
