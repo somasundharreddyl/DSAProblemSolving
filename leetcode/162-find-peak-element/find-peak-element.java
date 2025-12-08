a6e2f261d7206c796e4aee0d0554c0ba9bd04691
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-12-08 08:22:58 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (26.40%) - LeetSync

--- leetcode/162-find-peak-element/find-peak-element.java
+++ leetcode/162-find-peak-element/find-peak-element.java
@@ -3,23 +3,18 @@ class Solution {
         if(nums.length==1){
             return 0;
         }
-        if(nums[0]>nums[1]){
-            return 0;
-        }
-        if(nums[nums.length-1]>nums[nums.length-2]){
-            return nums.length-1;
-        }
-       int start=1,end=nums.length-2;
-       while(start<=end){
-           int mid=start+(end-start)/2;
-           if((nums[mid]>nums[mid-1])&&(nums[mid]>nums[mid+1])){
-               return mid;
-           }else if(nums[mid]>nums[mid+1]){
-               end=mid-1;
-           }else {
-               start=mid+1;
+        int start=0,end=nums.length-1;
+        while(start<=end){
+            int mid=start+(end-start)/2;
+           if((mid!=0 && mid!=nums.length-1 && nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) ||
+           (mid==0 && nums[mid]>nums[mid+1]) || (mid==nums.length-1 && nums[mid]>nums[mid-1])){
+            return mid;
+           }else if((mid!=nums.length-1 && nums[mid]<nums[mid+1]) ){
+            start=mid+1;
+           }else{
+            end=mid-1;
            }
-       }
-       return -1;
+        }
+        return -1;
     }
 }
