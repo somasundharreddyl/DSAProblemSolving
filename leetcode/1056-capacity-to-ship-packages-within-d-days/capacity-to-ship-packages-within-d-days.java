362a0423503c7dff62bbffecaa9ba3015c70bec4
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2025-12-10 09:34:27 +0530

    Time: 11 ms (73.24%) | Memory: 49.9 MB (51.55%) - LeetSync

--- leetcode/1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
+++ leetcode/1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.java
@@ -1,27 +1,34 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int max=0,min=Integer.MIN_VALUE,ans=-1;
-        for(int i=0;i<weights.length;i++){
-            max+=weights[i];
-            min=min>weights[i]?min:weights[i];
-        }
-        while(min<=max){
-            int mid=min+(max-min)/2;
-            int count=1,sum=0;
-            for(int i=0;i<weights.length;i++){
-                if(sum+weights[i]>mid){
-                 count++;
-                 sum=0;
-                }
-                sum=sum+weights[i];
-            }
-            if(count<=days){
-                ans=mid;
-                max=mid-1;         
-            }else{
-               min=mid+1;    
-            }
-        }
-        return ans;
+     int start=0,end=0;
+     int ans=0;
+     for(int i=0;i<weights.length;i++){
+        start=Math.max(weights[i],start);
+        end+=weights[i];
+     }  
+     while(start<=end){
+        int mid=start+(end-start)/2;
+        int estimationDays=calculateDays(weights,mid);
+        if(estimationDays<=days){
+            ans=mid;
+            end=mid-1;
+        }else{
+           start=mid+1;
+     }
+     }
+     return ans;
+}
+public int calculateDays(int[] weights,int weight){
+  int weightSum=0;
+  int dayCount=1;
+  for(int i=0;i<weights.length;i++){
+    if(weightSum+weights[i]>weight){
+        weightSum=0;
+        dayCount++;
     }
+        weightSum+=weights[i];
+    
+  }
+  return dayCount;
+}
 }
