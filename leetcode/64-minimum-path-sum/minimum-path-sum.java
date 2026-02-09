d17feefba16260d36e875576d01ccfd269c0339c
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2026-02-09 10:28:01 +0530

    Time: 1 ms (99.63%) | Memory: 49.8 MB (96.12%) - LeetSync

--- leetcode/64-minimum-path-sum/minimum-path-sum.java
+++ leetcode/64-minimum-path-sum/minimum-path-sum.java
@@ -1,22 +1,19 @@
 class Solution {
     public int minPathSum(int[][] grid) {
-      int[][] dp=new int[grid.length][grid[0].length];
-      for(int[] arr:dp){
-        Arrays.fill(arr,-1);
-      }
-      return minPathFinder(grid,dp,grid.length-1,grid[0].length-1); 
+       int[][] dp=new int[grid.length][grid[0].length];
+       for(int[] d:dp){
+        Arrays.fill(d,-1);
+       } 
+       return minimumPathFinder(grid,dp,grid.length-1,grid[0].length-1);
     }
-    public int minPathFinder(int[][] grid,int[][] dp,int i, int j){
-      if(i<0 || j<0){
-        return Integer.MAX_VALUE;
-      }  
-      if(i==0 && j==0){
-        return grid[i][j];
-      }
-      if(dp[i][j]!=-1){return dp[i][j];}
-      int x=minPathFinder(grid,dp,i,j-1);
-      int y=minPathFinder(grid,dp,i-1,j);
-      dp[i][j]=Math.min(x,y)+grid[i][j];
-      return Math.min(x,y)+grid[i][j];
+
+    public int minimumPathFinder(int[][] grid,int[][] dp,int i,int j){
+        if(i<0 || j<0){return Integer.MAX_VALUE;}
+        if(i==0 && j==0){return grid[i][j];}
+        if(dp[i][j]!=-1){return dp[i][j];}
+        int x=minimumPathFinder(grid,dp,i-1,j);
+        int y=minimumPathFinder(grid,dp,i,j-1);
+        dp[i][j]=Math.min(x,y)+grid[i][j];
+        return Math.min(x,y)+grid[i][j];
     }
 }
