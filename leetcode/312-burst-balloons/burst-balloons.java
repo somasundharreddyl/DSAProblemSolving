e3cb58a889fe5eafa5644e750d981014581f29e6
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2026-03-02 07:19:42 +0530

    Time: 103 ms (49.33%) | Memory: 44.6 MB (47.90%) - LeetSync

--- leetcode/312-burst-balloons/burst-balloons.java
+++ leetcode/312-burst-balloons/burst-balloons.java
@@ -1,33 +1,31 @@
 class Solution {
-    public int maxCoins(int[] nums) {
-        int[] arr=new int[nums.length+2];
-        arr[0]=1;
-        int k=1;
-        for(int i=0;i<nums.length;i++){
-            arr[k]=nums[i];
-            k++;
-        }
-        arr[arr.length-1]=1;
-        int start=0,end=arr.length;
-        int[][] dp=new int[arr.length][arr.length];
-        for(int[] d:dp){
-            Arrays.fill(d,-1);
-        }
-       return burstBallons(arr,start,end-1,dp);
-    }
-    public int burstBallons(int[] arr,int start,int end,int[][] dp){
-             if(start+1>=end){
-                return 0;
-             }
-             if(dp[start][end]!=-1){return dp[start][end];}
-             int ans=Integer.MIN_VALUE;
-             for(int i=start+1;i<end;i++){
-                int left=burstBallons(arr,start,i,dp);
-                int right=burstBallons(arr,i,end,dp);
-                ans=Math.max(ans,left+right+arr[start]*arr[end]*arr[i]); 
-             }
-             dp[start][end]=ans;
-             return ans;
-
+      public int maxCoins(int[] nums) { 
+        int n = nums.length; 
+        int[] arr = new int[n+2]; 
+        arr[0] = 1; 
+        arr[n+1] = 1; 
+        for(int i = 0; i<n; i++){ 
+            arr[i+1] = nums[i]; 
+        } 
+        int[][] dp = new int[n+2][n+2]; 
+        for(int [] d : dp){ 
+            Arrays.fill(d,-1); 
+        } 
+         return burstballon_rec(arr , 0, n+1,dp); 
+         
+    } 
+    public int burstballon_rec(int[] arr , int s , int e, int[][] dp){ 
+        if(s+1 >= e) return 0; 
+        if(dp[s][e] != -1) return dp[s][e]; 
+        int ans = Integer.MIN_VALUE; 
+        for(int i = s+1; i<e; i++){ 
+            int leftans = burstballon_rec(arr , s , i,dp); 
+            int rightans = burstballon_rec(arr , i , e,dp); 
+            int myans = leftans+rightans + arr[s]*arr[i]*arr[e]; 
+           ans = Math.max(ans , myans); 
+        } 
+        dp[s][e] = ans; 
+        return ans; 
+         
     }
 }
