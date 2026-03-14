481b93dc92aac4bfdafe0929151bdc0ef5187ae6
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2026-03-14 08:59:01 +0530

    Time: 2 ms (90.52%) | Memory: 44.4 MB (23.20%) - LeetSync

--- leetcode/1036-rotting-oranges/rotting-oranges.java
+++ leetcode/1036-rotting-oranges/rotting-oranges.java
@@ -1,54 +1,56 @@
 class Solution {
     public int orangesRotting(int[][] grid) {
+        Queue<Pair> q=new LinkedList<>();
         int ans=0;
-      Queue<Pair> q=new LinkedList<>();
-      for(int i=0;i<grid.length;i++){
-        for(int j=0;j<grid[0].length;j++){
-            if(grid[i][j]==2){
-                q.add(new Pair(i,j,0));
+        int m=grid.length;
+        int n=grid[0].length;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==2){
+                    q.add(new Pair(i,j,0));
+                }
             }
         }
-      }
-      while(q.size()>0){
-        Pair pair=q.remove();
-        int i=pair.i;
-        int j=pair.j;
-        int t=pair.t;
-        ans=t;
-        if(i-1>=0 && grid[i-1][j]==1){
-            grid[i-1][j]=2;
-            q.add(new Pair(i-1,j,t+1));
-        }
-        if(j-1>=0 && grid[i][j-1]==1){
-            grid[i][j-1]=2;
-            q.add(new Pair(i,j-1,t+1));
-        }
-        if(i+1<grid.length && grid[i+1][j]==1){
-            grid[i+1][j]=2;
-            q.add(new Pair(i+1,j,t+1));
-        }
-        if(j+1<grid[0].length && grid[i][j+1]==1){
-            grid[i][j+1]=2;
-            q.add(new Pair(i,j+1,t+1));
-        }
-      } 
-     for(int i=0;i<grid.length;i++){
-        for(int j=0;j<grid[0].length;j++){
-            if(grid[i][j]==1){
-                return -1;
+        while(q.size()>0){
+            Pair rem=q.remove();
+            int x=rem.x;
+            int y=rem.y;
+            int time=rem.time;
+            ans=time;
+            if(y-1>=0 && grid[x][y-1]==1){
+                grid[x][y-1]=2;
+                q.add(new Pair(x,y-1,time+1));
+            }
+            if(x-1>=0 && grid[x-1][y]==1){
+                grid[x-1][y]=2;
+                q.add(new Pair(x-1,y,time+1));
+            }
+            if(x+1<m && grid[x+1][y]==1){
+                grid[x+1][y]=2;
+                q.add(new Pair(x+1,y,time+1));
+            }
+            if(y+1<n && grid[x][y+1]==1){
+                grid[x][y+1]=2;
+                q.add(new Pair(x,y+1,time+1));
             }
         }
-      }
-      return ans;  
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1){
+                    return -1;
+                }
+            }
+        }
+        return ans;
     }
 }
 class Pair{
-    int i;
-    int j;
-    int t;
-    public Pair(int i,int j,int t){
-        this.i=i;
-        this.j=j;
-        this.t=t;
+    int x;
+    int y;
+    int time;
+    public Pair(int x,int y,int time){
+      this.x=x;
+      this.y=y;
+      this.time=time;
     }
 }
