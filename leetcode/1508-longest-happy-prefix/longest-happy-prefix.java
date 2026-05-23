886d3803d2163acf449fb039e95710cfb0842984
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2026-05-23 12:11:21 +0530

    Time: 7 ms (95.96%) | Memory: 48 MB (9.31%) - LeetSync

---
+++ leetcode/1508-longest-happy-prefix/longest-happy-prefix.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String longestPrefix(String s) {
+       int n=s.length();
+        int[] happyPrefixArr=new int[n]; //0,0,1,0,0,0,0
+        char[] charArr=s.toCharArray();
+        for(int i=1;i<n;i++){
+          int idx=happyPrefixArr[i-1]; //1
+          if(charArr[i]==charArr[idx]){ 
+            happyPrefixArr[i]=idx+1;
+          }else{
+            while(idx-1>=0){
+             if(charArr[i]==charArr[happyPrefixArr[idx-1]]){
+               happyPrefixArr[i]=happyPrefixArr[idx-1]+1;
+               break;
+             }
+             idx=happyPrefixArr[idx-1];
+            }
+          }
+        }
+        return s.substring(0,happyPrefixArr[n-1]);
+    }
+}
