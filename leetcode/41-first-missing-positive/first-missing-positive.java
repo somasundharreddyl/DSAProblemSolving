b54dcce7ca4405bf6c49fbaeef26f685d9fee5a8
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2026-06-02 08:17:11 +0530

    Time: 1 ms (100.00%) | Memory: 70.8 MB (95.41%) - LeetSync

--- leetcode/41-first-missing-positive/first-missing-positive.java
+++ leetcode/41-first-missing-positive/first-missing-positive.java
@@ -1,19 +1,20 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
+        int min=1;
+        int max=nums.length;
         int n=nums.length;
         for(int i=0;i<n;i++){
-            while(nums[i]>=1 && nums[i]<=n && (i+1)!=nums[i] && nums[i]!=nums[nums[i]-1]){
-                int num=nums[i];
-               int temp=nums[num-1];
-               nums[num-1]=nums[i];
-               nums[i]=temp;
-            }   
+            while(nums[i]>=min && nums[i]<=max && nums[i]!=(i+1) && nums[i]!=nums[nums[i]-1]){
+                int temp=nums[i]; 
+                nums[i]=nums[temp-1];
+                nums[temp-1]=temp;
+            }
         }
         for(int i=0;i<n;i++){
-            if(i+1!=nums[i]){
+            if((i+1)!=nums[i]){
                 return i+1;
             }
         }
-        return n+1;
+        return max+1;
     }
 }
