d20d09f8a8ca675b2747f066b002d97403676057
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2026-06-03 10:49:18 +0530

    Time: 3 ms (73.82%) | Memory: 52.8 MB (27.78%) - LeetSync

--- leetcode/229-majority-element-ii/majority-element-ii.java
+++ leetcode/229-majority-element-ii/majority-element-ii.java
@@ -1,39 +1,42 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> ans=new ArrayList<>();
-        int freq1=0,freq2=0,ans1=Integer.MIN_VALUE,ans2=Integer.MIN_VALUE;  //freq1=0,ans1=2,   freq2=1,ans2=1
-        for(int i=0;i<nums.length;i++){
-            if((freq1==0 || ans1==nums[i]) && ans2!=nums[i]){
-                freq1++;
-                ans1=nums[i];
-                continue;
-            }
-            if(freq2==0 || ans2==nums[i]){ //
+        int n=nums.length;
+        int num1=Integer.MIN_VALUE,num2=Integer.MIN_VALUE;
+        int freq1=0,freq2=0;
+        for(int i=0;i<n;i++){
+            if((freq1==0 || nums[i]==num1) && num2!=nums[i]){
+              num1=nums[i];
+              freq1++;
+              continue;
+            }else if(freq2==0 || nums[i]==num2){
+                num2=nums[i];
                 freq2++;
-                ans2=nums[i];
                 continue;
             }
-            if(nums[i]!=ans1 && nums[i]!=ans2){
-                freq1--;
-                freq2--;
+            freq1--;
+            freq2--;
+        }
+        int count1=0,count2=0;
+        if(freq1>0){
+        for(int i=0;i<n;i++){
+            if(nums[i]==num1){
+                count1++;
             }
         }
-        int c1=0,c2=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==ans1){
-                c1++;
+        }
+        if(freq2>0){
+        for(int i=0;i<n;i++){
+            if(nums[i]==num2){
+                count2++;
             }
         }
-        if(c1>nums.length/3){
-            ans.add(ans1);
         }
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==ans2){
-                c2++;
-            }
+        List<Integer> ans=new ArrayList<>();
+        if(count1>n/3){
+            ans.add(num1);
         }
-        if(c2>nums.length/3){
-            ans.add(ans2);
+        if(count2>n/3){
+            ans.add(num2);
         }
         return ans;
     }
