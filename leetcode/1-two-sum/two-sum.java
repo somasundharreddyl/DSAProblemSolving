484aa2ff95c70eb5649d04ab6646aa61724e85d3
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2026-06-11 07:58:33 +0530

    Time: 7 ms (42.77%) | Memory: 47.7 MB (11.02%) - LeetSync

--- leetcode/1-two-sum/two-sum.java
+++ leetcode/1-two-sum/two-sum.java
@@ -5,31 +5,28 @@ class Solution {
             arr[i]=nums[i];
         }
         Arrays.sort(arr);
-        int i=0,j=arr.length-1;
-        int[] ans=new int[]{-1,-1};
-        while(i<j){
-           if(arr[i]+arr[j]==target){
-             ans[0]=i;
-             ans[1]=j;
-             break;
-           }else if(arr[i]+arr[j]>target){
-             j--;
-           }else{
-            i++;
-           }
+        int ele1=-1,ele2=-1;
+        int start=0,end=arr.length-1;
+        while(start<end){
+            int sum=arr[start]+arr[end];
+            if(sum==target){
+               ele1=arr[start];
+               ele2=arr[end];
+               break;
+            }else if(sum<target){
+              start++;
+            }else{
+                end--;
+            } 
         }
-        for(int k=0;k<nums.length;k++){
-            if(nums[k]==arr[ans[0]]){
-                ans[0]=k;
-                break;
+        int idx1=-1,idx2=-1;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==ele1 && idx1==-1){
+               idx1=i;
+            }else if(nums[i]==ele2){
+               idx2=i;
             }
         }
-        for(int k=0;k<nums.length;k++){
-            if(ans[0]!=k && nums[k]==arr[ans[1]]){
-                ans[1]=k;
-                break;
-            }
-        }
-        return ans;
+        return new int[]{idx1,idx2};
     }
 }
