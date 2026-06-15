9dff9f54469b28e9d9b170b0072495dbc79de45f
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2026-06-15 09:06:24 +0530

    Time: 1 ms (93.95%) | Memory: 42.3 MB (91.57%) - LeetSync

--- leetcode/29-divide-two-integers/divide-two-integers.java
+++ leetcode/29-divide-two-integers/divide-two-integers.java
@@ -1,27 +1,25 @@
 class Solution {
     public int divide(int dividend, int divisor) {
+        long k=divisor,n=dividend;
         int sign=1;
-        long sum=0,q=0;
-        if(dividend<0){sign=sign*-1;}
-        if(divisor<0){sign=sign*-1;}
-        long a=dividend;
-        long b=divisor;
-        a=(Math.abs(a));
-        b=(Math.abs(b));
-        for(int i=31;i>=0;i--){
-           if(sum+(b<<i)<=a){
-            sum=sum+(b<<i);
-            q=q+((long)1<<i);
-           } 
+        if(k<0){sign*=-1;}
+        if(n<0){sign*=-1;}
+        k=Math.abs(k);
+        n=Math.abs(n);
+        long ans=0,sum=0;
+    for(int i=31;i>=0;i--){
+        if((sum+(k<<i))<=n){
+            ans+=((long)1<<i);
+            sum+=(k<<i);
         }
-        if(sign<0){
-            q=-q;
-        }
-        if(q>Integer.MAX_VALUE){
-            return Integer.MAX_VALUE;
-        }else if(q<Integer.MIN_VALUE){
-           return Integer.MIN_VALUE;
-        }
-        return (int)q;
+    }
+    if(sign<0){ans*=sign;}
+    if(ans>Integer.MAX_VALUE){
+        ans=Integer.MAX_VALUE;
+    }
+    if(ans<Integer.MIN_VALUE){
+        ans=Integer.MIN_VALUE;
+    }
+    return (int)ans;    
     }
 }
