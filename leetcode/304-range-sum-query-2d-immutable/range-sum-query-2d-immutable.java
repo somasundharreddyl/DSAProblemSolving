5b52bc921493abd52b7c9e2e5cccf867045f7ca1
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2026-07-02 08:24:07 +0530

    Time: 100 ms (88.63%) | Memory: 141.6 MB (76.38%) - LeetSync

--- leetcode/304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
+++ leetcode/304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.java
@@ -8,32 +8,35 @@ class NumMatrix {
         sumMatrix=new int[r][c];
         for(int i=0;i<r;i++){
             for(int j=0;j<c;j++){
-               if(j==0){
-                sumMatrix[i][j]=matrix[i][j];
-               }else{
-                sumMatrix[i][j]=sumMatrix[i][j-1]+matrix[i][j];
-               } 
+                if(j==0){
+                 sumMatrix[i][j]=matrix[i][j];
+                }else{
+                 sumMatrix[i][j]=sumMatrix[i][j-1]+matrix[i][j];  
+                }
             }
         }
         for(int i=0;i<c;i++){
-            for(int j=1;j<r;j++){
-                sumMatrix[j][i]+=sumMatrix[j-1][i];
+            for(int j=0;j<r;j++){
+                if(j==0){
+                 sumMatrix[j][i]=sumMatrix[j][i];
+                }else{
+                 sumMatrix[j][i]+=sumMatrix[j-1][i];
+                }
             }
-        } 
+        }
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum=sumMatrix[row2][col2];
-        if(row1>0){
-            sum-=sumMatrix[row1-1][col2];
+        int total=sumMatrix[row2][col2];
+        if(row1==0 && col1==0){
+            return total;
+        }else if(row1==0){
+            return total-sumMatrix[row2][col1-1];
+        }else if(col1==0){
+            return total-sumMatrix[row1-1][col2];
+        }else{
+            return total-sumMatrix[row2][col1-1]-sumMatrix[row1-1][col2]+sumMatrix[row1-1][col1-1];
         }
-        if(col1>0){
-            sum-=sumMatrix[row2][col1-1];
-        }
-        if(row1>0 && col1>0){
-            sum+=sumMatrix[row1-1][col1-1];
-        }
-        return sum;
     }
 }
 
