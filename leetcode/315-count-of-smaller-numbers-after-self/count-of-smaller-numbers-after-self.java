b92ef22aeb62324f9ac45519beff85665e601cd8
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2026-07-12 15:04:05 +0530

    Time: 103 ms (38.78%) | Memory: 108.3 MB (31.48%) - LeetSync

--- leetcode/315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.java
+++ leetcode/315-count-of-smaller-numbers-after-self/count-of-smaller-numbers-after-self.java
@@ -1,52 +1,56 @@
-class Pair{ 
-    int ele; 
-    int idx; 
-    Pair(int ele , int idx){ 
-        this.ele = ele; 
-        this.idx = idx; 
-    }     
-}
-class Solution { 
-    public List<Integer> countSmaller(int[] nums) { 
-        int n = nums.length; 
-        int[] count = new int[n]; 
-        Pair[] arr = new Pair[n]; 
-        for(int i = 0; i<n; i++){ 
-            arr[i] = new Pair(nums[i],i); 
-        } 
-        mergeSort(arr,count,0,n-1); 
-        List<Integer> ans = new ArrayList<>(); 
-        for(int ele : count) ans.add(ele); 
-        return ans; 
+class Solution {
+    public List<Integer> countSmaller(int[] nums) {
+       List<Integer> counts=new ArrayList<>(Collections.nCopies(nums.length, 0));
+       Pair[] pairs=new Pair[nums.length]; 
+       for(int i=0;i<nums.length;i++){
+         pairs[i]=new Pair(nums[i],i);
+       }
+       splitArr(pairs,0,nums.length-1,counts);
+       return counts; 
     }
-     public void mergeSort(Pair[] arr,int[] count,int l,int r){ 
-        if(l>=r) return; 
-        int m = (l+r)/2; 
-        mergeSort(arr,count,l,m); 
-        mergeSort(arr,count,m+1,r); 
-        mergeTwoSorted(arr,count,l,m,r); 
-    } 
-    public void mergeTwoSorted(Pair[] nums,int[] count,int l,int m,int r){ 
-        int s1 = m-l+1; 
-        int s2 = r-m; 
-        Pair[] arr1 = new Pair[s1]; 
-        Pair[] arr2 = new Pair[s2]; 
-        for(int i = 0; i<s1; i++){ 
-            arr1[i] = nums[i+l]; 
-        } 
-        for(int i = 0; i<s2; i++){ 
-            arr2[i] = nums[i+m+1]; 
-        } 
-        int i = 0,j = 0,k = l; 
-        while(i<s1 && j<s2){ 
-            if(arr1[i].ele>arr2[j].ele){ 
-                count[arr1[i].idx] += s2-j; 
-                nums[k++] = arr1[i++]; 
-            } else { 
-                nums[k++] = arr2[j++]; 
-            } 
-        } 
-        while(i<s1) nums[k++] = arr1[i++]; 
-        while(j<s2) nums[k++] = arr2[j++];    
-    } 
-}  
+    public void splitArr(Pair[] pairs,int s, int e, List<Integer> counts){
+       if(s>=e){
+        return;
+       }
+       int m=s+(e-s)/2;
+       splitArr(pairs,s,m,counts);
+       splitArr(pairs,m+1,e,counts);
+       mergeArr(pairs,s,m,e,counts);
+    }
+    public void mergeArr(Pair[] pairs,int s, int m, int e, List<Integer> counts){
+         Pair[] temp=new Pair[e-s+1];
+         int s1=s,s2=m+1,i=0;
+         while(s1<=m && s2<=e){
+            if(pairs[s1].value>pairs[s2].value){
+               int idx=pairs[s1].idx;
+               counts.set(idx,counts.get(idx)+e-s2+1);
+               temp[i++]=pairs[s1++];
+            }else{
+               temp[i++]=pairs[s2++];
+            }
+         }
+         while(s1<=m){
+        //    int idx=pairs[s1].idx;
+        //    counts.set(idx,counts.get(idx)+m-s1+1);
+           temp[i++]=pairs[s1++];
+         }
+         while(s2<=e){
+        //    int idx=pairs[s2].idx;
+        //    counts.set(idx,counts.get(idx)+e-s1+1);
+           temp[i++]=pairs[s2++];
+         }
+         int k=0;
+         for(int j=s;j<=e;j++){
+            pairs[j]=temp[k++];
+         }
+    }
+
+}
+class Pair{
+    int value;
+    int idx;
+    public Pair(int value, int idx){
+        this.value=value;
+        this.idx=idx;
+    }
+}
