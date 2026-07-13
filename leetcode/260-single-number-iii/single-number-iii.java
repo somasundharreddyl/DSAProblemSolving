edea6daebaf1f224534210e7f27f6f04bd2c0b8b
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2026-07-13 12:56:08 +0530

    Time: 1 ms (100.00%) | Memory: 48.1 MB (40.41%) - LeetSync

--- leetcode/260-single-number-iii/single-number-iii.java
+++ leetcode/260-single-number-iii/single-number-iii.java
@@ -1,22 +1,33 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
-        int xorValue=nums[0];
-        int setBit=0,unSetBit=0;
-        for(int i=1;i<nums.length;i++){
-           xorValue=xorValue^nums[i];
+        int xorVal=nums[0];
+        int n=nums.length;
+        for(int i=1;i<n;i++){
+            xorVal=xorVal^nums[i];
         }
+        int idx=-1;
         for(int i=0;i<32;i++){
-            if((xorValue&(1<<i))!=0){
-                for(int j=0;j<nums.length;j++){
-                   if((nums[j]&(1<<i))==0){
-                    setBit=setBit^nums[j];
-                   }else{
-                    unSetBit=unSetBit^nums[j];
-                   }
+          if((xorVal&(1<<i))!=0){
+            idx=i;
+            break;
+          }
+        }
+        int part0=-1,part1=-1;
+        for(int i=0;i<n;i++){
+            if((nums[i]&(1<<idx))==0){
+                if(part0==-1){
+                    part0=nums[i];
+                }else{
+                part0=part0^nums[i];
                 }
-              return new int[]{setBit,unSetBit};  
+            }else{
+                if(part1==-1){
+                    part1=nums[i];
+                }else{
+                part1=part1^nums[i];
+                }
             }
         }
-        return new int[]{-1,-1};
+        return new int[]{part0,part1};
     }
 }
