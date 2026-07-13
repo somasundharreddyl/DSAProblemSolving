fd8cb027a450ed7dd0edf45a5ca944a53214cccb
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2026-07-13 08:34:19 +0530

    Time: 7 ms (45.78%) | Memory: 47.6 MB (10.96%) - LeetSync

--- leetcode/1-two-sum/two-sum.java
+++ leetcode/1-two-sum/two-sum.java
@@ -5,28 +5,29 @@ class Solution {
             arr[i]=nums[i];
         }
         Arrays.sort(arr);
-        int ele1=-1,ele2=-1;
-        int start=0,end=arr.length-1;
-        while(start<end){
-            int sum=arr[start]+arr[end];
-            if(sum==target){
-               ele1=arr[start];
-               ele2=arr[end];
-               break;
-            }else if(sum<target){
-              start++;
+        int s=0,e=arr.length-1;
+        int n1=-1,n2=-1;
+        while(s<e){
+            if(arr[s]+arr[e]==target){
+              n1=arr[s];
+              n2=arr[e];
+              break;
+            }
+           else if(arr[s]+arr[e]>target){
+                e--;
             }else{
-                end--;
-            } 
-        }
-        int idx1=-1,idx2=-1;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==ele1 && idx1==-1){
-               idx1=i;
-            }else if(nums[i]==ele2){
-               idx2=i;
+                s++;
             }
         }
-        return new int[]{idx1,idx2};
+       int idx1=Integer.MIN_VALUE,idx2=Integer.MIN_VALUE;
+       for(int i=0;i<nums.length;i++){
+        if(idx1==Integer.MIN_VALUE && nums[i]==n1){
+               idx1=i;
+        }
+        if(idx2==Integer.MIN_VALUE && nums[i]==n2 && idx1!=i){
+            idx2=i;
+        }
+       }
+       return new int[]{idx1,idx2};
     }
 }
