8098ea86049d0aea5b12dd2a9dcb3034abe38f97
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2026-07-21 19:26:13 +0530

    Time: 29 ms (30.65%) | Memory: 51.8 MB (14.30%) - LeetSync

--- leetcode/85-maximal-rectangle/maximal-rectangle.java
+++ leetcode/85-maximal-rectangle/maximal-rectangle.java
@@ -1,74 +1,72 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        int[][] arr=new int[matrix.length][matrix[0].length];
-        for(int i=0;i<matrix.length;i++){
-            for(int j=0;j<matrix[i].length;j++){
-               if(i==0){
-                if(matrix[i][j]=='1'){
-                   arr[i][j]=1;
+        int m=matrix.length;
+        int n=matrix[0].length;
+        int[][] arr=new int[m][n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]=='0'){
+                arr[i][j]=0;
                 }else{
-                   arr[i][j]=0;
+                arr[i][j]=1;
                 }
-               }else{
-               if(matrix[i][j]=='1'){
-                   arr[i][j]=arr[i-1][j]+1;
-                }else{
-                   arr[i][j]=0;
-                }
-               }
             }
         }
-        int ans=0;
-        for(int i=0;i<arr.length;i++){
-         int res=largestRectangleArea(arr[i]);
-         ans=ans>res?ans:res;
-        }
-        return ans;
-    }
+        int maxArea=Integer.MIN_VALUE;
+        for(int i=0;i<m;i++){
+            Stack<Integer> stack=new Stack<>();
+            for(int j=0;j<n;j++){
+                if(i==0){
+                  if(stack.isEmpty()){
+                    stack.push(j);
+                    continue;
+                  }
+                  while(stack.size()>0 && arr[i][stack.peek()]>=arr[i][j]){
+                    int h=stack.pop();
+                    int x2=j;
+                    int x1=-1;
+                    if(stack.size()>0){
+                     x1=stack.peek();
+                    }
+                    int area=((x2-1)-(x1+1)+1)*arr[i][h];
+                    maxArea=Math.max(area,maxArea);
+                  }
+                  stack.push(j);  
+                }else{
+                  if (matrix[i][j] == '1') {
+                      arr[i][j] += arr[i-1][j];
+                    } else {
+                        arr[i][j] = 0;
+                     }  
+                  if(stack.isEmpty()){
+                    stack.push(j);
+                    continue;
+                  }
+                  while(stack.size()>0 && arr[i][stack.peek()]>=arr[i][j]){
+                    int h=stack.pop();
+                    int x2=j;
+                    int x1=-1;
+                    if(stack.size()>0){
+                     x1=stack.peek();
+                    }
+                    int area=((x2-1)-(x1+1)+1)*arr[i][h];
+                    maxArea=Math.max(area,maxArea);
+                  }
+                  stack.push(j);
+                }
+            }
 
-    public int largestRectangleArea(int[] heights) {
-        int[] lsn=new int[heights.length];
-        int[] rsn=new int[heights.length];
-        Arrays.fill(lsn,-1);
-        Arrays.fill(rsn,-1);
-        Stack<Integer> leftStack=new Stack<>();
-        Stack<Integer> rightStack=new Stack<>();
-        for(int i=0;i<heights.length;i++){
-            if(rightStack.size()==0){
-               rightStack.push(i); 
-               continue;
-            }
-            while(rightStack.size()>0 && heights[rightStack.peek()]>heights[i]){
-                rsn[rightStack.pop()]=i;
-            }
-            rightStack.push(i);
+            while(stack.size()>0){
+                    int h=stack.pop();
+                    int x2=n;
+                    int x1=-1;
+                    if(stack.size()>0){
+                     x1=stack.peek();
+                    }
+                    int area=((x2-1)-(x1+1)+1)*arr[i][h];
+                    maxArea=Math.max(area,maxArea);
+                }
         }
-        for(int i=heights.length-1;i>=0;i--){
-            if(leftStack.size()==0){
-               leftStack.push(i); 
-               continue;
-            }
-            while(leftStack.size()>0 && heights[leftStack.peek()]>heights[i]){
-                lsn[leftStack.pop()]=i;
-            }
-            leftStack.push(i);
-        }
-        int ans=0;
-        for(int i=0;i<heights.length;i++){
-            if(lsn[i]<0 && rsn[i]<0){
-               int width=heights.length;
-               ans=ans>(heights[i]*width)?ans:(heights[i]*width); 
-            }else if(lsn[i]>=0 && rsn[i]>0){
-               int width=(rsn[i]-1)-(lsn[i]+1)+1;
-               ans=ans>(heights[i]*width)?ans:(heights[i]*width);
-            }else if(lsn[i]>=0 && rsn[i]<0){
-               int width=(heights.length-1)-(lsn[i]+1)+1;
-               ans=ans>(heights[i]*width)?ans:(heights[i]*width);
-            }else{
-                int width=rsn[i];
-               ans=ans>(heights[i]*width)?ans:(heights[i]*width); 
-            }
-        }
-       return ans;
+        return maxArea;
     }
 }
