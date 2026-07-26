065d89244a0961d082fcf6cfe2d511f48d3655dc
SUNDHAR <127641469+somasundharreddyl@users.noreply.github.com>
2026-07-26 19:10:33 +0530

    Time: 2 ms (87.39%) | Memory: 46.3 MB (45.09%) - LeetSync

--- leetcode/983-validate-stack-sequences/validate-stack-sequences.java
+++ leetcode/983-validate-stack-sequences/validate-stack-sequences.java
@@ -1,30 +1,23 @@
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
         Stack<Integer> stack=new Stack<>();
-        int i=0,j=0;
-        while(i<pushed.length){
-            if(stack.size()==0){
+        int k=0;
+        for(int i=0;i<pushed.length;i++){
+            if(stack.isEmpty()){
                 stack.push(pushed[i]);
-                i++;
-                continue;
+                continue;   
             }
-            if(popped[j]==stack.peek()){
-              stack.pop();
-              j++;
-            }else{
-                stack.push(pushed[i]);
-                i++;
-            }
-        }
 
-        while(stack.size()>0){
-            if(popped[j]==stack.peek()){
-              stack.pop();
-              j++;
-            }else{
-                return false;
+            while(!stack.isEmpty() && popped[k]==stack.peek()){
+               stack.pop();
+               k++;
             }
+            stack.push(pushed[i]);
         }
-        return true;
+        while(!stack.isEmpty() && popped[k]==stack.peek()){
+               stack.pop();
+               k++;
+        }
+        return stack.isEmpty();
     }
 }
